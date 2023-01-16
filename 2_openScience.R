80981d255439f28efa55316b67a225625096c017
klapierre <kjkomatsu@uncg.edu>
2023-01-16 16:27:04 -0500

    Section 1: Finding open data.

--- 2_openScience.R
+++ 2_openScience.R
@@ -6,3 +6,29 @@
 ## Branch from a GitHub repository and practice staging, committing, and pull/pushing code. 
 ## Create a resource to refer back to.
 
+# ----------------------------------------------------------
+#### 1) Finding and interpreting open data.####
+# ----------------------------------------------------------
+
+## Go to the following website and download the full data package
+## (halfway down the page): https://portal.edirepository.org/nis/mapbrowse?packageid=knb-lter-knz.148.3
+
+## Extract (unzip) the files into a folder named "YourLastName_conSME_data".
+
+
+## QUESTION: Open up the file "knb-lter-knz.148.3.txt". What does this file contain?
+
+
+## QUESTION: From this file, who are the dataset contributors?
+
+
+## QUESTION: From this file, what are the start and end dates of the dataset?
+
+
+## QUESTION: Is the data collection still ongoing?
+
+
+## QUESTION: Given your answer to the previous question, why might it be good to
+## have a reproducible script for data analysis related to this dataset?
+
+
