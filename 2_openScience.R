2a7780f559ceb76e8b6fee1fcdb47258d81b87f4
klapierre <kjkomatsu@uncg.edu>
2023-01-16 16:24:17 -0500

    Setting lesson objectives.

---
+++ 2_openScience.R
@@ -0,0 +1,8 @@
+#### MODULE 2: Open Data, Project Management, and GitHub #### 
+
+## OBJECTIVE:
+## Know where to find data and how to bring it into the R environment.
+## Set up a good file management system for yourself, which you can build on in the future.
+## Branch from a GitHub repository and practice staging, committing, and pull/pushing code. 
+## Create a resource to refer back to.
+
