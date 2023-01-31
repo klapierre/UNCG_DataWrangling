8d27f0187b205b8869b508387069304c71612140
klapierre <kjkomatsu@uncg.edu>
2023-01-31 18:51:02 -0500

    Selecting columns.

--- 4_wrangle.R
+++ 4_wrangle.R
@@ -88,3 +88,72 @@ streamTempRename <- rename(.data=streamTemp,
 # In this code, does the new column name come before or after the =?
 
 
+# ---------------------------------------------------------- #
+### PART 1.2: SELECTING COLUMNS                           ####
+# ---------------------------------------------------------- #
+
+# The select() function allows you to pick columns by name. This can be helpful when
+# you want to clean a larger dataset and focus on your variables of interest.
+# 
+# Let's imagine that we are only interested in the temperature at the Calispell 
+# site.
+
+# TASK: Look again at the columns you have in the streamTempRename dataframe by
+# writing the necessary code below.
+
+
+# TASK: Run the following line of code to select our columns of interest.
+calispellTemp <- select(.data=streamTempRename,
+                        calispell_temp, Date, Time)
+
+
+# QUESTION: Take a look at the column names for our new dataframe (calispellTemp),
+# by coding the appropriate R function of course. What do you notice about the 
+# new dataframe? Which columns are present? Which are absent? Are they in the same 
+# order as before?
+
+
+# A nice thing to notice about this code. We didn't have to type 'streamTemp$date'
+# etc to indicate each column as we would outside of the tidyverse. The select()
+# function knows we are referring to streamTemp because the dataframe is our first
+# argument to the function.
+
+
+# TASK: Recall that in R, the `:` operator is a compact way to create a sequence of
+# numbers. For example, write the code below to generate a sequence from 1 to 3.
+# HINT: Look back to assignment #1 or the swirl tutorial for help (or google!).
+
+
+# Normally this notation is just for numbers, but the select() function allows you
+# to specify a sequence of columns this way. This can save a bunch of typing!
+
+#TASK: Create a new dataframe called calispellTemp2 and select the date, time, and
+# calispell_temp columns using the sequence notation.
+# HINT: Replace the code where each column was listed out with a sequence of column
+# names. Be sure they are listed in the order they exist in the original dataframe.
+
+
+# TASK: Write code to check your column names again to see what happened in your
+# new dataframe.
+
+
+# We can also specify the columns that we want to discard by selecting them out.
+# TASK: Run the following code to remove the Smalle_temp and Winchester_temp 
+# columns. Note the use of the - sign to remove columns.
+calispellTemp3 <- select(.data=streamTempRename,
+                         -Smalle_temp, -Winchester_temp)
+
+# The same thing can be accomplished by running the following code, which
+# concatenates these columns together.
+calispellTemp4 <- select(.data=streamTempRename,
+                         -c(Smalle_temp, Winchester_temp))
+
+# And the same thing can ALSO be accomplished by running the following code, which
+# subtracts out a sequence of columns.
+calispellTemp5 <- select(.data=streamTempRename,
+                         -Smalle_temp:-Winchester_temp)
+
+
+# TASK: Write code to check that these two new dataframes (calispellTemp3,  calispellTemp4, and calispellTemp5 are identical).
+
+
