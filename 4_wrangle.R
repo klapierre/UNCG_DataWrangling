e6dcefdd43c8800ca346f12881d4434a724ae596
Kimberly Komatsu <KJKOMATSU@uncg.edu>
2023-01-31 17:41:46 -0500

    Assignment set up.

---
+++ 4_wrangle.R
@@ -0,0 +1,25 @@
+# ---------------------------------------------------------- #
+#### MODULE 2: Wrangle some data!                         ####               
+# ---------------------------------------------------------- #
+
+## OBJECTIVE:
+# 1. To learn how to manipulate and transform data into a form usable for analysis and graphs.
+# 2. To do this in a way that each step is traceable and reproducible.
+# To this end we'll be familiarizing ourselves with and diving into the dplyr package.
+
+
+# ---------------------------------------------------------- #
+#### SET UP:                                              ####
+# ---------------------------------------------------------- #
+# REMINDER: Create a new branch for this week through the GitHub website
+# called yourLastName_week4. Open the Rproject for our course
+# (UNCG_DataWrangling),pull any changes from GitHub, and navigate to the
+# branch you created for this week in RStudio's Git tab. Open this week's
+# assignment in this new branch to complete and submit on GitHub.
+# Ideally, you'll commit your answers a bit at a time as you work through
+# this assignment, rather than committing all at once at the end.
+
+
+# The dplyr package is nested within the tidyverse package (along with many others).
+# Install tidyverse and load the library. HINT: see the end of assignment #1 if you 
+# forgot how to install and load a package.
