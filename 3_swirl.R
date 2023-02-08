a77263831284d165ada52dcce1171a8bb70f103c
Kim Komatsu <kjkomatsu@uncg.edu>
2023-02-08 15:52:25 -0500

    Moving from week 4 branch, which is being deleted.

--- 3_swirl.R
+++ 3_swirl.R
@@ -40,7 +40,7 @@
 # you left off. 
 
 # If swirl is not yet installed, install it first
-# install.packages("swirl")
+install.packages("swirl")
 # Then load swirl
 library(swirl)
 # Then install the course R Programming
@@ -55,22 +55,25 @@ swirl()
 # ---------------------------------------------------------- #
 # QUESTION: What would you do to get back a line of code you previously ran
 # from your console without retyping the whole thing again?
-
+#you can hit the up arrow to go back to a previous line of code
 
 # ---------------------------------------------------------- #
 #### II. R Programming -> 2. Workspace and Files          #### 
 # ---------------------------------------------------------- #
 # QUESTION: What is the difference between your working directory
 # and your local workspace?
+#your working dorectory is where all your files get saved and where you can insert files from
 
 # QUESTION: How do you find help files for a function using R code?
-
+#type ? before the function, like ?file.create
 
 # ---------------------------------------------------------- #
 #### III. R Programming -> 3. Sequences of Numbers        #### 
 # ---------------------------------------------------------- #
 # QUESTION: In R, is there usually just one way or many different
 # ways of doing the same thing? Why might this be useful?
+#There are multiple ways to do the sme thing typically
+#it is a plus becuase it may be helpful if you forget one way to do something but remember another
 
 
 # ---------------------------------------------------------- #
@@ -80,15 +83,15 @@ swirl()
 # logical vector, a character vector, an integer vector, and a 
 # numeric vector.
 
-# QUESTION: What does <= stand for?
+# QUESTION: What does <= stand for? equality
 
-# QUESTION: What does == stand for?
+# QUESTION: What does == stand for? equality
 
-# QUESTION: What does != stand for?
+# QUESTION: What does != stand for? inequality
 
-# QUESTION: What does | mean in R?
+# QUESTION: What does | mean in R? logical or aka union
 
-# QUESTION: What does & mean when testing for TRUE/FALSE statements?
+# QUESTION: What does & mean when testing for TRUE/FALSE statements? whether both are true
 
 
 # ---------------------------------------------------------- #
@@ -96,7 +99,11 @@ swirl()
 # ---------------------------------------------------------- #
 
 # TASK: Copy your assignment of my_data below, then repeat the code 
-# that generated my_data to create my_data2. 
+# that generated my_data to create my_data2.
+my_data <- sample(c(y, z), 100)
+my_data2 <- sample(c(y, z), 100)
+identical(my_data, my_data2)
+
 # Using the R function identical(), test whether my_data and my_data2 
 # are identical.
 # Hint: You can always Google or in R type ?identical() to get
@@ -104,17 +111,19 @@ swirl()
 
 
 # QUESTION: Explain why my_data and my_data2 were or were not identical.
-
+#The sample function pulls a random sample from within the sampling criteria so the two vectors did not have the same sample
 
 # ---------------------------------------------------------- #
 #### VI. R Programming -> 6. Subsetting                   ####
 # ---------------------------------------------------------- #
 # QUESTION: Subsetting can be an amazing thing! Describe one real world
 # scenario where you might want to subset a dataset. (you can make up anything)
+#subsetting vectors could be useful when selecting data that is positive
 
 # QUESTION: Sometimes R gives you an answer to a question that shouldn't be
 # asked (e.g., when you asked for the 3000th variable in your vector x). Why
 # might this be an issue?
+#if NA could be an option for a variable then you wouldn't know if the variable doesn't exist or if that is the correct value
 
 
 # ---------------------------------------------------------- #
@@ -122,18 +131,21 @@ swirl()
 # ---------------------------------------------------------- #
 # QUESTION: In the code matrix[x,y], which letter represents the rows? 
 # Which letter represents the columns?
-
+#x is rows, y is columns
 
 # QUESTION: What are two different ways you could assign the numbers 1-20
 # into a matrix with 4 rows and 5 columns?
+#my_vector <- 1:20
+#dim(my_vector) <- c(4, 5)
 
+#my_matrix2 <- matrix(data=1:20, nrow=4, ncol=5)
 
 # ---------------------------------------------------------- #
 #### VIII. R Programming -> 8. Logic                      ####
 # ---------------------------------------------------------- #
 # TASK: Leave a comment that you have completed this section. 
 # Did you feel like you were taking an LSAT?
-
+# I took a practice LSAT for fun once and did horribly. Thats how this felt
 
 # ---------------------------------------------------------- #
 #### VII. R Programming -> 15. Base Graphics              ####
@@ -141,15 +153,25 @@ swirl()
 
 # TASK: dim(), names(), head(), and tail() are all great functions!
 # Run each of them on the cars dataset.
+data(cars)
+dim(cars)
+names(cars)
+head(cars)
+tail(cars)
 
 # QUESTION: In your own words, describe what each of these functions do and
 # why each one might be useful.
+#dim: shows the dimensions of the data
+#names: shows the names of the two variabls
+#head: shows the top 6 rows
+#tail: shows the bottom 6 rows
 
 # TASK: Run the str() function on the cars dataset.
+str(cars)
 
 # QUESTION: In your own words, describe what the str() function does.
 # Why might this be useful?
-
+#str lists the number of objects, variables, and top values for said variables. This is a good way to get a basic overall summary of the data
 
 # ---------------------------------------------------------- #
 #### PART 2: TEST WHAT YOU LEARNED                        ####
@@ -164,7 +186,7 @@ b <- 4
 a <- b
 c <- a
 c
-
+#it prints 4. It runs in order down the page
 
 # II) Three of the following lines produce the same result. 
 # Without running the code, which one will produce a different result than the
@@ -175,6 +197,8 @@ log10(1000)
 log(base = 10, x = 1000)
 log(10, 1000)
 
+?log
+#log(10, 1000) is different because it is improperly formatted
 
 
 # III) Use one of R's built in functions to create a new vector that is 
@@ -182,25 +206,28 @@ log(10, 1000)
 # this function, but a combination of Google and function documentation 
 # should get you there.
 numvector <- c(5,2,3,1,6,8)
-
+decending <- sort(numvector, decreasing = TRUE)
+decending
 
 # IV) Which elephant weighs more? 
 # Convert one’s weight to the units of the other, and store the result in an 
 # appropriately-named new variable. Test whether elephant1 weights more than 
 # elephant2 using an equation that returns either true or false (1 kg ≈ 2.2 lb).
 
-elephant1_kg <- 3492
+elephant1_lb <- 3492 * 2.2
 elephant2_lb <- 7757
-
+elephant1_lb == elephant2_lb
 
 # ---------------------------------------------------------- #
 #### PART 3: FEEDBACK                                     ####
 # ---------------------------------------------------------- #
 # QUESTION: What did you think of the Swirl tutorial? Was it helpful? Why 
 # or why not?
+#I thought it was okay but not the most helpful. I think it would have been more helpful with more information on why we did certain things and more examples.
 
 # QUESTION: Would you have preferred to split the Swirl tutorial over weeks
 # 1 and 3 of the class instead of the week 1 assignment you previously 
 # completed? Why or why not?
+#I would keep them separate. Since this course is opwn to beginners, I think the first assignment was a great way to get them used to the program
 
 # REMEMBER: Save and push your script when you're done with this assignment!
