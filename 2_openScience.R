a011b5cb1b9acc90a9d4b6383779c94a180eadc1
Kim Komatsu <kjkomatsu@uncg.edu>
2023-02-08 15:36:24 -0500

    Moving from week 3 branch, which is being deleted.

--- 2_openScience.R
+++ 2_openScience.R
@@ -17,19 +17,22 @@
 
 
 ## QUESTION: Open up the file "knb-lter-knz.148.3.txt". What does this file contain?
-
+# This file contains Contributors, their contact information, and the data format
 
 ## QUESTION: From this file, who are the dataset contributors?
 
+#Kimberly Komatsu, Meghan Avolio, Andrew Hope, Sally Koerner, Allison Louthan, and Kevin Wilcox
 
 ## QUESTION: From this file, what are the start and end dates of the dataset?
 
+#1-1-2019 to 12-31-2021
 
 ## QUESTION: Is the data collection still ongoing?
-
+#Maintenence is ongoing
 
 ## QUESTION: Given your answer to the previous question, why might it be good to
 ## have a reproducible script for data analysis related to this dataset?
+#It may be a good idea to have a reproducible script in order to plug in yearly data to get comparable figures
 
 
 # ----------------------------------------------------------
@@ -39,17 +42,19 @@
 ## We can use the .gitignore file to tell git what files not to sync to GitHub!
 
 ## QUESTION: What are two reasons why we wouldn't want GitHub to sync this data?
-# GitHub is not good at storing data.
+# GitHub is not good at storing data. This data may contain your raw data.
 
 
 ## TASK: Check the Git tab in RStudio. Do you see your data folder listed added?
 ## Now, open the .gitignore file from the files tab in RStudio.
 ## Tell git to ignore the entire folder containing the data you just downloaded.
+
 ## Save the .gitignore file.
 
 
 ## QUESTION: What happened to the data folder listed in the Git tab of RStudio
 ## when you hit save?
+#The folder disappeared from the Git tab
 
 
 ## TASK: Stage, commit, and pull/push your modified .gitignore file to the branch
@@ -65,6 +70,7 @@
 ## TASK: Start by setting your working directory to the GitHub repository folder
 ## for this class on your computer using the function setwd().
 ## Hint - set the working directory as the top folder.
+setwd("D://School Stuff/Graduate Stuff/Graduate Classes/Spring 2023/UNCG_DataWrangling/Mann_conSME_data")
 
 
 ## TASK: Now we can import one of these datasets into R. Let's import the plant
@@ -73,28 +79,30 @@
 ## a name that includes the experiment name (conSME) and the data type (abundance)
 ## using '<-' and be sure to carefully consider your naming convention when doing so.
 
+CME011<-read.csv("D://School Stuff/Graduate Stuff/Graduate Classes/Spring 2023/UNCG_DataWrangling/Mann_conSME_data/CME011.csv")
 
 ## Run the following code.
 conSMEcoverAlt <- read.csv("https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-knz.148.3&entityid=5716ee946efd717292fa3da9241cda7c")
 
 
 ## QUESTION: What did this code do? What can you say about these two dataframes?
-
+#This code found and downloaded a dataframe from the internet. These two dataframes seem identical on first glance.
 
 ## TASK: Check to see if the two dataframes are identical using an R function.
 ## (Hint: remember the Week 1 assignment?)
-
+identical(CME011, conSMEcoverAlt)
+#TRUE
 
 ## QUESTION: Why might it be better to source data straight from the data portal?
 ## Why might it be worse?
-
+#The data may be constantly updated, always giving you up-to-date information. This may be an issue, as it may cause tables or figures to function incorrectly.
 
 ## TASK: Save your R script. Then stage, commit, and pull/push your
 ## modified code to the branch you created for this week with an appropriate 
 ## commit message. Remember, it is most effective to commit small chunks of code 
 ## often with specific commit messages!
 
-
+# Checkpoint 1 completed
 
 # ----------------------------------------------------------
 #### 4) Thinking through your naming conventions.####
@@ -102,28 +110,30 @@ conSMEcoverAlt <- read.csv("https://portal.edirepository.org/nis/dataviewer?pack
 
 ## TASK: Check the names of the columns in the species cover dataset you imported.
 ## (Hint: remember the Week 1 assignment?)
-
+colnames(CME011)
 
 ## QUESTION: What naming convention did the dataset creators use for column names?
+#camelCase naming convention
 
 
 ## QUESTION: What naming convention do you plan to use for this course for the
 ## following types of objects in R:
-## R scripts
-## vectors
-## dataframes
-## columns within dataframes
-## homemade functions
+## R scripts- Snake_case
+## vectors- Snake_case
+## dataframes- camelCase
+## columns within dataframes- camelCase
+## homemade functions- Snake_case
 
 
 ## QUESTION: Do all of your objects follow the same naming convention or do you
 ## plan to use different naming conventions to reference different object types?
+#Differing naming conventions
 
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to the branch
 ## you created for this week with an appropriate commit message.
 
-
+# Openscience checkpoint 2
 
 # ----------------------------------------------------------
 #### 5) Opening Issues.####
@@ -136,15 +146,19 @@ conSMEcoverAlt <- read.csv("https://portal.edirepository.org/nis/dataviewer?pack
 ## TASK: Create a new dataframe named "duplicates" by binding the rows of two 
 ## conSME dataframes you made into one using the rbind() function.
 ## (Hint: Very similar to the cbind function we used in the Week 1 assignment)
+duplicates<-rbind(CME011, conSMEcoverAlt)
 
 
 ## QUESTION: Looking at the information for each dataframe in the environment tab
 ## of RStudio, what do you notice about the number of observations for the 
 ## duplicates dataframe compared to the two original dataframes?
+#The duplicates dataframs has double the number of observations, but the same number of variables as the CME files
 
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to the branch
 ## you created for this week with an appropriate commit message.
+
+#Checkpoint 3 reached
 
 
 ## TASK: Open the duplicates dataframe by clicking on the file name in the 
@@ -156,11 +170,13 @@ duplicates[with(duplicates, order(RecDate, Block, Plot, Taxa)),]
 
 ## QUESTION: What do you notice about the data? Specifically, compare rows 
 ## 4934 and 12173 (if they are sorted correctly, those should be on top).
+#The data is duplicated, and these two cells are identical
 
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to the branch
 ## you created for this week with an appropriate commit message.
 
+#Checkpoint 4 reached
 
 ## It looks like we have a problem (that we created for ourselves)! Let's open an
 ## issue to make note of this.
@@ -181,7 +197,7 @@ noDuplicates <- unique(duplicates)
 ## of RStudio, what do you notice about the number of observations for the 
 ## noDuplicates dataframe compared to the dulpicates dataframe? What about compared
 ## to the two original dataframes?
-
+#The number of observations in the dataframe is less than both original CME files. The nodupes dataframs more than halved the number of observations.
 
 ## TASK: Go back to our repository in GitHub through your web browser. Find the
 ## issue you created and resolve it.
@@ -189,6 +205,7 @@ noDuplicates <- unique(duplicates)
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to 
 ## the branch you created for this week with an appropriate commit message.
 
+# Checkpoint 5 reached
 
 
 # ----------------------------------------------------------
@@ -204,6 +221,7 @@ noDuplicates <- unique(duplicates)
 
 ## TASK: Type another comment below.
 
+#CHeckpoint 6 reached. Final push/ learning to amend step.
 
 ## TASK: Save your R script. Open up the commit window in RStudio.
 
@@ -211,6 +229,7 @@ noDuplicates <- unique(duplicates)
 ## QUESTION: How many commits are you ahead of your branch on GitHub?
 ## (Hint: look for the message "Your branch is ahead of..." near the top of the 
 ## window.)
+# 1 commit ahead
 
 
 ## TASK: Stage your modified code and check the "Amend previous commit" box.
@@ -219,6 +238,7 @@ noDuplicates <- unique(duplicates)
 
 ## QUESTION: What happened when you clicked "Amend previous commit"?
 ## How many commits is your branch ahead by now?
+#The former message reappeared and was able to be edited
 
 
 ## TASK: Once you've answered the above questions, save your R script one last time. 
