71ca24c6ed257089f9114a30b78963c50cb1f9d8
Kim Komatsu <kjkomatsu@uncg.edu>
2023-02-08 15:14:18 -0500

    Moving from week 3 branch, which is being deleted.

--- 2_openScience.R
+++ 2_openScience.R
@@ -17,20 +17,25 @@
 
 
 ## QUESTION: Open up the file "knb-lter-knz.148.3.txt". What does this file contain?
-
+##This file contains a dataset for a scientific experiment
 
 ## QUESTION: From this file, who are the dataset contributors?
-
+## Kimberly Komatsu, Meghan Avolio, Andrew Hope, Sally Koerner, Allison Louthan, Kevin Wilcox, Konza LTER
 
 ## QUESTION: From this file, what are the start and end dates of the dataset?
+##Start: 01/01/2019
+##End: 12/31/2021
 
 
 ## QUESTION: Is the data collection still ongoing?
+##Yes
 
 
 ## QUESTION: Given your answer to the previous question, why might it be good to
 ## have a reproducible script for data analysis related to this dataset?
-
+## Because the data collection is still ongoing, having a reproducible script for
+# data analysis would allow for a faster processing time of new data added to the
+# set.
 
 # ----------------------------------------------------------
 #### 2) Preventing GitHub from syncing the data files.####
@@ -40,21 +45,26 @@
 
 ## QUESTION: What are two reasons why we wouldn't want GitHub to sync this data?
 # GitHub is not good at storing data.
-
+# Our data may not be ready to sync to GitHub for others to access
 
 ## TASK: Check the Git tab in RStudio. Do you see your data folder listed added?
 ## Now, open the .gitignore file from the files tab in RStudio.
 ## Tell git to ignore the entire folder containing the data you just downloaded.
 ## Save the .gitignore file.
+## Eichhorn_conSME_data.gitignore
 
 
 ## QUESTION: What happened to the data folder listed in the Git tab of RStudio
 ## when you hit save?
+#The path was converted into .gitignore
 
 
 ## TASK: Stage, commit, and pull/push your modified .gitignore file to the branch
 ## you created for this week with an appropriate commit message.
-
+#Pull
+#Select .getignore
+#Commit
+#Push
 
 # ----------------------------------------------------------
 #### 3) Getting open data into R.####
@@ -65,28 +75,33 @@
 ## TASK: Start by setting your working directory to the GitHub repository folder
 ## for this class on your computer using the function setwd().
 ## Hint - set the working directory as the top folder.
-
+#setwd("Eichhorn_conSME_data")
 
 ## TASK: Now we can import one of these datasets into R. Let's import the plant
 ## species abundance datafile (CME011). To do so, use the read.csv() function, 
 ## putting the relative file path and file name. Assign the dataframe you import
 ## a name that includes the experiment name (conSME) and the data type (abundance)
 ## using '<-' and be sure to carefully consider your naming convention when doing so.
-
+#conSME_abundance <- read.csv("CME011.csv")
 
 ## Run the following code.
 conSMEcoverAlt <- read.csv("https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-knz.148.3&entityid=5716ee946efd717292fa3da9241cda7c")
 
 
 ## QUESTION: What did this code do? What can you say about these two dataframes?
-
+# One dataset is pulled from a direct online repository, one is pulled from a downloaded source
 
 ## TASK: Check to see if the two dataframes are identical using an R function.
 ## (Hint: remember the Week 1 assignment?)
+# identical("conSME_abundance","conSMEcoverAlt")
+# FALSE
 
 
 ## QUESTION: Why might it be better to source data straight from the data portal?
 ## Why might it be worse?
+# It is best to source data straight from the data portal to allow for you to be 
+# working with the most up-to-date data. However, one may wish to not go with this
+# route to avoid working with errors uploaded by others.
 
 
 ## TASK: Save your R script. Then stage, commit, and pull/push your
@@ -102,23 +117,30 @@ conSMEcoverAlt <- read.csv("https://portal.edirepository.org/nis/dataviewer?pack
 
 ## TASK: Check the names of the columns in the species cover dataset you imported.
 ## (Hint: remember the Week 1 assignment?)
-
+# colnames(conSME_abundance)
 
 ## QUESTION: What naming convention did the dataset creators use for column names?
+# The dataset creators used a combination of shorthand and to-the-point names to describe the data in each column
 
 
 ## QUESTION: What naming convention do you plan to use for this course for the
 ## following types of objects in R:
 ## R scripts
+##  For scripts, I usually use a combination of uppercase and lowercase, with underscores between words
 ## vectors
+## For vectors, I use shorthand lowercase
 ## dataframes
+## For dataframes, I use combinations of uppercase and lowercase
 ## columns within dataframes
+## For columns, I would use shorthand with Upper and Lower case
 ## homemade functions
+## For homemade functions, I would use straightforward, single words
 
 
 ## QUESTION: Do all of your objects follow the same naming convention or do you
 ## plan to use different naming conventions to reference different object types?
-
+## Not all objects should follow the same naming conventions, sometimes the
+## object names will be longer than necessary, so they need to be shortened.
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to the branch
 ## you created for this week with an appropriate commit message.
@@ -136,12 +158,13 @@ conSMEcoverAlt <- read.csv("https://portal.edirepository.org/nis/dataviewer?pack
 ## TASK: Create a new dataframe named "duplicates" by binding the rows of two 
 ## conSME dataframes you made into one using the rbind() function.
 ## (Hint: Very similar to the cbind function we used in the Week 1 assignment)
+# duplicates <- rbind(conSME_abundance,conSMEcoverAlt)
 
 
 ## QUESTION: Looking at the information for each dataframe in the environment tab
 ## of RStudio, what do you notice about the number of observations for the 
 ## duplicates dataframe compared to the two original dataframes?
-
+## The number of observations has been combined
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to the branch
 ## you created for this week with an appropriate commit message.
@@ -156,6 +179,7 @@ duplicates[with(duplicates, order(RecDate, Block, Plot, Taxa)),]
 
 ## QUESTION: What do you notice about the data? Specifically, compare rows 
 ## 4934 and 12173 (if they are sorted correctly, those should be on top).
+## The data should be exact duplicates of eachother.
 
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to the branch
@@ -169,7 +193,7 @@ duplicates[with(duplicates, order(RecDate, Block, Plot, Taxa)),]
 ## TASK: Go to our repository in GitHub through your web browser. Click on issues
 ## and start a new issue. Give it a descriptive title that references the script
 ## name and line number of the issue. The write a comment describing the issue.
-
+## COMPELTED
 
 ## Now let's fix our issue!
 ## A great function to get rid of exact duplicate columns in R is unique().
@@ -181,10 +205,12 @@ noDuplicates <- unique(duplicates)
 ## of RStudio, what do you notice about the number of observations for the 
 ## noDuplicates dataframe compared to the dulpicates dataframe? What about compared
 ## to the two original dataframes?
+## noDuplicates has removed all exact replications of the data in the original dataframes, while duplicates has exact replications
 
 
 ## TASK: Go back to our repository in GitHub through your web browser. Find the
 ## issue you created and resolve it.
+## COMPLETED
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to 
 ## the branch you created for this week with an appropriate commit message.
@@ -196,6 +222,7 @@ noDuplicates <- unique(duplicates)
 # ----------------------------------------------------------
 
 ## TASK: Type a comment below.
+## Howdy, there!
 
 
 ## TASK: Follow these instructions carefully!
@@ -203,7 +230,7 @@ noDuplicates <- unique(duplicates)
 ## but this time DON'T pull/push your modified code!
 
 ## TASK: Type another comment below.
-
+## Yeehaw
 
 ## TASK: Save your R script. Open up the commit window in RStudio.
 
@@ -211,10 +238,12 @@ noDuplicates <- unique(duplicates)
 ## QUESTION: How many commits are you ahead of your branch on GitHub?
 ## (Hint: look for the message "Your branch is ahead of..." near the top of the 
 ## window.)
+# 1 commit
 
 
 ## TASK: Stage your modified code and check the "Amend previous commit" box.
 ## Then commit your code (still don't pull/push).
+
 
 
 ## QUESTION: What happened when you clicked "Amend previous commit"?
