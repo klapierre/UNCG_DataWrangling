2ba29dcded70d9924ca563906b0a6aa0955110d1
Kimberly Komatsu <KJKOMATSU@uncg.edu>
2023-02-09 16:00:49 -0500

    Setting up the lesson and final github reminder.

---
+++ 5_transform.R
@@ -0,0 +1,39 @@
+# ---------------------------------------------------------- #
+#### MODULE 2: Transform some data!                         ####               
+# ---------------------------------------------------------- #
+
+## OBJECTIVE:
+# 1. To learn how to manipulate and transform data into a form usable for analysis and graphs.
+# 2. To do this in a way that each step is traceable and reproducible.
+# To this end we'll continue to familiarize ourselves with the dplyr package and dive into the tidyr package.
+
+
+# ---------------------------------------------------------- #
+#### SET UP:                                              ####
+# ---------------------------------------------------------- #
+# REMINDER: In RStudio, open the Rproject for our course (UNCG_DataWrangling), 
+# pull any changes from GitHub, and navigate to the branch you created for 
+# yourself (just a single branch for the whole semester) in RStudio's Git tab.
+# Open this week's assignment in this new branch to complete and submit on GitHub.
+# Ideally, you'll commit your answers a bit at a time as you work through
+# this assignment, rather than committing all at once at the end.
+
+
+# REMINDER: The dplyr and tidyr packages are nested within the tidyverse package
+# (along with many others). Be sure to start by loading the tidyverse library.
+# HINT: see the end of assignment #1 if you forgot how to load a package.
+
+
+# ---------------------------------------------------------- #
+#### PART 1.0: LEARNING THE FUNCTIONS                     ####
+# ---------------------------------------------------------- #
+
+
+
+
+
+
+
+
+
+# REMINDER: If you haven't already, make sure to commit and push your code to your branch in GitHub!
