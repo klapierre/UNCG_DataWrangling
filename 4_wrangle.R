0284b1705fc7865cd2d5b76ef7934e4f51c62fe8
jalynbottomley <jhbottom@uncg.edu>
2023-02-12 13:43:45 -0500

    1.5

--- 4_wrangle.R
+++ 4_wrangle.R
@@ -299,18 +299,20 @@ calispellTempFaquatic
 
 # TASK: Write code to create one more column named ecosystem in a new dataframe and 
 # fill it with the word 'stream'.
-
-
+calispellTempF3 <- mutate(.data=calispellTempFaquatic,
+                          ecosystem='stream')
 # Now we might want to create a new column that includes information from both of
 # the columns we just created. We would do so by running the following lines of code:
 calispellTempF4 <- unite(data=calispellTempF3,
                          col='type_ecosystem',
                          c('type', 'ecosystem'),
                          sep='::')
-
+calispellTempF4
 # QUESTION: Describe in your own words what the code above does. What part creates
 # a new column? What part tells R which columns to combine? What does the sep= mean?
-
+#The code above separates the columns into their own structures.
+#The col= creates a new column.
+#sep= puts the two colons between the two names in the contents of that column created.
 
 # Another very useful function is separate, which takes apart a column into two or
 # more pieces.
@@ -320,11 +322,11 @@ calispellTempF5 <- separate(data=calispellTempF4,
                             col=type_ecosystem,
                             into=c('type', 'ecosystem'),
                             sep='::')
-
+calispellTempF5
 # QUESTION: Why isn't the column name in quotes this time?
-
+#Because the column was already created.
 # QUESTION: Describe in your own words what the code above does.
-
+#The code took away the colons in the column for type ecosystem.
 
 # ---------------------------------------------------------- #
 ### PART 1.6: PIPES                                       ####
