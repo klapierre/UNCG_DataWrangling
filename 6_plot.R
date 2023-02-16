c68ecb6a4721f5ade047f85a73f18e259fe63668
Kimberly Komatsu <KJKOMATSU@uncg.edu>
2023-02-16 12:30:18 -0500

    Assignment set up.

---
+++ 6_plot.R
@@ -0,0 +1,68 @@
+# ---------------------------------------------------------- #
+#### MODULE 4: Plot some data!                            ####               
+# ---------------------------------------------------------- #
+
+## OBJECTIVE:
+# 1. To develop a "grammar of graphics".
+# 2. To become comfortable using ggplot2 for exploratory data visualization.
+
+
+# ---------------------------------------------------------- #
+#### SET UP:                                              ####
+# ---------------------------------------------------------- #
+# REMINDER: In RStudio, open the Rproject for our course (UNCG_DataWrangling), 
+# pull any changes from GitHub, and navigate to the branch you created for 
+# yourself (just a single branch for the whole semester) in RStudio's Git tab.
+# Open this week's assignment in this new branch to complete and submit on GitHub.
+# Ideally, you'll commit your answers a bit at a time as you work through
+# this assignment, rather than committing all at once at the end.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# REMINDER: The ggplot2 package is nested within the tidyverse package
+# (along with many others). Be sure to start by loading the tidyverse library.
+# HINT: see the end of assignment #1 if you forgot how to load a package.
+library("tidyverse")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# REMEMBER: Save and push your script when you're done with this assignment!
