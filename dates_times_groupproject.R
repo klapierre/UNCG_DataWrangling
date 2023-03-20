6289026a8e5d417ae3698a6758515ddf47ae3990
rachbrenn <rach.brenneman@gmail.com>
2023-03-20 16:39:51 -0400

    making project file

---
+++ dates_times_groupproject.R
@@ -0,0 +1,14 @@
+#start of the dates and times
+
+#Add working directory
+setwd("/Users/rachaelbrenneman/Desktop/data_wrangle/UNCG_DataWrangling")
+
+##############################Tentative Outline################################
+#moving from 12hr to 24hr back and forth
+#moving from month,day,year to day,month,year
+#tidy data with dates (people write them completely differently)
+#plotting with dates
+
+
+########################Possible Resources#####################################
+https://rstudio-pubs-static.s3.amazonaws.com/704461_02220f30fffd427e9357c541024446a0.html
