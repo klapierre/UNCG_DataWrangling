4f01972941ba3b2f0ef2e2d68a37bb31bd69ba8b
CBravoC <bm.nina@gmail.com>
2023-03-20 17:00:08 -0400

    Update

--- MAP GROUP FILE.R
+++ MAP GROUP FILE.R
@@ -8,14 +8,15 @@
 #let's try it
 
 #Section 2
-#dataset -> USArrests
+#dataset -> USArrests (part of base R)
+#heat map (population data?)
 #script to enter the data points into the maps
 #wrap up question
 #make a map different from the default
 
 #Section 3
-#dataset (NYC starbucks locations)
-#making shapes/points
+#dataset (NYC starbucks locations) https://data.world/data-hut/starbucks-location-dataset
+#making shapes/points https://cran.r-project.org/web/packages/tidygeocoder/readme/README.html
 #wrap up question
 #make a map change it around
 
@@ -24,3 +25,8 @@
 #dataset TBD
 #Wrap up question
 #make a map, change settings
+
+#Conclusion
+#global distribution of something, heatmap
+
+#bigger problem where all skills listed above will be used
