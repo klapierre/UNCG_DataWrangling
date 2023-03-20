10a052325a2facf5fb8ac13374b63c06c115d568
William Mann <wtmann99@gmail.com>
2023-03-20 17:05:03 -0400

    st

--- dates_times_groupproject.R
+++ dates_times_groupproject.R
@@ -2,7 +2,7 @@
 
 #Add working directory
 setwd("/Users/rachaelbrenneman/Desktop/data_wrangle/UNCG_DataWrangling")
-
+setwd("D:/School Stuff/Graduate Studd/Graduate Classes/Spring 2023/UNCG_DataWrangling")
 ##############################Tentative Outline################################
 #moving from 12hr to 24hr back and forth
 #moving from month,day,year to day,month,year
@@ -15,3 +15,7 @@ https://rstudio-pubs-static.s3.amazonaws.com/704461_02220f30fffd427e9357c5410244
 https://r4ds.had.co.nz/dates-and-times.html
 https://www.r-bloggers.com/2020/04/a-comprehensive-introduction-to-handling-date-time-in-r/
   https://www.stat.berkeley.edu/~s133/dates.html#:~:text=Dates%20and%20Times%20in%20R%20R%20provides%20several,dates%20and%20times%20with%20control%20for%20time%20zones.
+
+date_time_untidy <-read.csv("date_times_very_untidy.csv")
+
+
