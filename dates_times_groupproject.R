29e3df0e88c69e16265055347508266c38f7b719
rachbrenn <rach.brenneman@gmail.com>
2023-03-20 16:43:34 -0400

    adding links

--- dates_times_groupproject.R
+++ dates_times_groupproject.R
@@ -12,3 +12,5 @@ setwd("/Users/rachaelbrenneman/Desktop/data_wrangle/UNCG_DataWrangling")
 
 ########################Possible Resources#####################################
 https://rstudio-pubs-static.s3.amazonaws.com/704461_02220f30fffd427e9357c541024446a0.html
+https://r4ds.had.co.nz/dates-and-times.html
+https://www.r-bloggers.com/2020/04/a-comprehensive-introduction-to-handling-date-time-in-r/
