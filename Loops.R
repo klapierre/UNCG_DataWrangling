f7741f6b4d2c5a2d4e4c388ada718059e2e6ca31
wolfmanwillie <nyxwolfram213@gmail.com>
2023-03-20 17:19:42 -0400

    PROPOSED WORKFLOW

--- Loops.R
+++ Loops.R
@@ -1,1 +1,37 @@
 # This is the file for the loops exercises and explanations
+
+
+#What are loops (Zachary)
+
+
+#For Loops 
+#Description (Lowie)
+#Example (Mason)
+#Do it yourself (William)
+
+#DUE: MARCH 26TH
+
+#While Loops
+#Description (Mason)
+#Next  (William)
+#Example (Zachary)
+#Do it yourself (Lowie)
+
+#DUE: MARCH 24TH
+
+#Repeat Loops
+#Description (William)
+#Break (Mason)
+#Example (Lowie)
+#Do it yourself (Zachary)
+
+#DUE: APRIL 5TH
+
+#Final 
+#Nested loops within For Loops 
+#Description of nested loops  (Zachary)
+#Example (William)
+#Do it yourself with a dataset (Mason)
+
+
+#DUE: APRIL 8TH
