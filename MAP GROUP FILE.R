d378cb48d650fb73ff998ab5ff56ef56c3ffe2de
CBravoC <bm.nina@gmail.com>
2023-03-20 17:05:09 -0400

    Updated

--- MAP GROUP FILE.R
+++ MAP GROUP FILE.R
@@ -1,32 +1,31 @@
 # MAP GROUP
 
-#Section 1
+#Section 1 (Carina)
 #Libraries to be downloaded
-#dataset -> country, states and county
+#data set -> country, states and county
 #Description of each library
 #Description of how to render the US map
 #let's try it
 
-#Section 2
-#dataset -> USArrests (part of base R)
+#Section 2 (Kaysa)
+#data set -> USArrests (part of base R)
 #heat map (population data?)
 #script to enter the data points into the maps
 #wrap up question
 #make a map different from the default
 
-#Section 3
-#dataset (NYC starbucks locations) https://data.world/data-hut/starbucks-location-dataset
+#Section 3 (Stephanie)
+#data set (NYC starbucks locations) https://data.world/data-hut/starbucks-location-dataset
 #making shapes/points https://cran.r-project.org/web/packages/tidygeocoder/readme/README.html
 #wrap up question
 #make a map change it around
 
-#Section 4
+#Section 4 (Jordan)
 #3D map
 #dataset TBD
 #Wrap up question
 #make a map, change settings
 
-#Conclusion
-#global distribution of something, heatmap
-
+#Conclusion (Group)
+#global distribution of something, heat map
 #bigger problem where all skills listed above will be used
