bc6ac2f47bac68746e7d01fd493e438ed560b907
CBravoC <bm.nina@gmail.com>
2023-03-20 16:43:14 -0400

    Sharing

--- MAP GROUP FILE.R
+++ MAP GROUP FILE.R
@@ -1,1 +1,26 @@
 # MAP GROUP
+
+#Section 1
+#Libraries to be downloaded
+#dataset -> country, states and county
+#Description of each library
+#Description of how to render the US map
+#let's try it
+
+#Section 2
+#dataset -> USArrests
+#script to enter the data points into the maps
+#wrap up question
+#make a map different from the default
+
+#Section 3
+#dataset (NYC starbucks locations)
+#making shapes/points
+#wrap up question
+#make a map change it around
+
+#Section 4
+#3D map
+#dataset TBD
+#Wrap up question
+#make a map, change settings
