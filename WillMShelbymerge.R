edb2fee491c1b937b90b29e4ece4b7e7a3f6c636
Ethan-Isaac1997 <eiisaac@uncg.edu>
2023-03-20 16:29:17 -0400

    mm

---
+++ WillMShelbymerge.R
@@ -0,0 +1,64 @@
+<<<<<<< HEAD
+
+
+
+
+
+
+#Lets break R together!
+
+#we are testing breaking R! Let's start!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Mwah ahhahahahahahahahahahaaaaa!!!!!
+#It is I! The secret line of Code!!!!
+# Mweheheheheheheeee!
+
+
+
+
+
+#i win
+
+=======
+#i win
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# Jordan wuz here
+>>>>>>> 23596a37299c2e047f6038212711b7330faf929d
