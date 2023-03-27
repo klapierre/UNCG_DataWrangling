46279d0a3e4e57734ed9ae9197792be0bd57580b
shelbywilliford <sbwilliford@uncg.edu>
2023-03-27 16:53:04 -0400

    attempt 2

--- Group_project_my_section.R
+++ Group_project_my_section.R
@@ -4,7 +4,8 @@ install.packages('gifski')
 library(gifski)
 titanic <- read.csv("titanic.csv")
 
-p <- ggplot(mtcars, aes(factor(cyl), mpg)) + 
+mtcars
+p <- ggplot(mtcars, aes(factor(hp), mpg)) + 
   geom_bar(width = 1, stat = "identity") + 
   coord_polar("y", start=0) +
   transition_states(
@@ -14,6 +15,23 @@ p <- ggplot(mtcars, aes(factor(cyl), mpg)) +
   ) +
   enter_fade() + 
   exit_shrink() +
-  ease_aes('sine-in-out')
+  ease_aes('bounce-in')
 pq <- animate(p, renderer = gifski_renderer())
-anim_save('filename.gif', animation = last_animation(), path = NULL)
+anim_save('filename2.gif', animation = last_animation(), path = NULL)
+
+tit1 <- titanic %>% 
+  ggplot(x="", y= Age)) + 
+  geom_bar(width = 1, stat = "identity") + 
+  coord_polar("y", start=0) 
+tit1
+  
+  transition_states(
+    gear,
+    transition_length = 2,
+    state_length = 1
+  ) +
+  enter_fade() + 
+  exit_shrink() +
+  ease_aes('bounce-in')
+titanic_chart <- animate(tit1, renderer = gifski_renderer())
+anim_save('titanic1.gif', animation = last_animation(), path = NULL)
