0994cf12347d0b9037828394f2909e874bdb5cf6
CBravoC <bm.nina@gmail.com>
2023-03-27 17:27:44 -0400

    Map 1

--- Map_Section1.R
+++ Map_Section1.R
@@ -12,24 +12,25 @@
 #The RStudio "maps" package is useful for creating maps and visualizing geographic data. It gives you access to a wide range of map data, such as political boundaries, topography, and even satellite imagery, which you can use to create custom maps based on your specific needs. 
 
 #To use the "maps" package in RStudio, first install it in the R console by typing 
-install.packages("maps")
+install.packages('maps')
+install.packages('mapproj')
 
 #Once installed, use the command to load the package into your workspace.
 library(maps)
+library(mapproj)
+
+#For example, you could use the following code to generate a basic map of the world using the "Mercator" projection:
+map("world", projection = "mercator")
+
+#In addition to basic map creation, the "maps" package includes functions for adding points, lines, and text to maps, as well as custom legends and color scales.
+
+
+#Overall, the RStudio "maps" package is a powerful tool for creating maps and visualizing geographic data that can be used in a variety of applications ranging from data analysis and visualization to education and research.
 
 #Rstudio can be used for more than tidying and transforming data. Lets try create a map of the United States ggplot2 and other packages. First, install or load the following libraries:
-library(ggplot2)
-Library(tidyverse)
-library(ggmap)
-library(mapdata)
-library(maps)
-library(maptools)
-library(stringr)
-library(dplyr)
 
 #
-library(maps)
-map("world", projection = "mercator")
+
 
 #Load the map data for the United States by typing the following command into the R console:
 
@@ -55,10 +56,19 @@ geom_polygon(data = state, aes(x = long, y = lat, group = group), fill = "white"
 geom_point(data = cities, aes(x = long, y = lat), size = 1, color = "red") +
 labs(title = "US Map with Major Cities")
 
+
 #This will plot the US map with red dots representing the location of major cities.
 #Conclusion (Group)
 #global distribution of something, heat map
 #bigger problem where all skills listed above will be used
+#library(ggplot2)
+#library(tidyverse)
+#library(ggmap)
+#library(mapdata)
+#library(maps)
+#library(maptools)
+#library(stringr)
+#library(dplyr)
 
 
 
