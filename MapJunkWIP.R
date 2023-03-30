ff2b85209ba5aaae4c8c6c80d941dd89cfdf79bf
jordanwinter <jawinter2@uncg.edu>
2023-03-30 00:36:38 -0400

    file upkeep.

---
+++ MapJunkWIP.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(ggplot2)
+library(ggmap)
+library(maps)
+library(mapdata)
+
+# Ok for this last part of the assignment we are going to brush up on our geography.
+# Don't get too excited. ;)
+
+# We first need to make a base map that is the entire earth.
+# Go ahead and do the same process as you did above but use "world" instead.
+
+?map
+
+world <- map_data("world")
+
+ggplot() + geom_polygon(data = world, aes(x=long, y = lat, group = group)) + 
+  coord_fixed(1.3) 
+worldmap <-map(database = "world")
+
+ggplot() + geom_polygon(data = world, aes(x=long, y = lat, group = group), fill = "blue", col = "black") + 
+  coord_fixed(1.3) + geom_point(data = caps, aes(x = Longitude, y = Latitude), color = "yellow", size = 1)
+
+abline(x=1)
+# Nice what was your output?
+
+# Answer: It better be a map
+
+# Ok now practice changing the outline and fill colors to something other than the default.
+
+# The same as above but ya know. A little different and most likely uglier.
+
+caps <- read.csv("country-capital-lat-long-population.csv")
+
