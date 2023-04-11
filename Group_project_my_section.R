49c926e554db3043e99d7aa4b98db27fb6ae9cf6
shelbywilliford <sbwilliford@uncg.edu>
2023-04-11 13:56:14 -0400

    First graph that has worked!

--- Group_project_my_section.R
+++ Group_project_my_section.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(gganimate)
 library(gifski)
-titanic <- read.csv("titanic.csv")
+library(RColorBrewer)
 
 mtcars
 p <- ggplot(mtcars, aes(factor(cyl), mpg)) + 
@@ -29,3 +29,22 @@ tic <- ggplot(titanic, aes(factor(Pclass), Survived)) +
   ease_aes('sine-in-out')
 tic1 <- animate(tic, renderer = gifski_renderer())
 anim_save('tic.gif', animation = last_animation(), path = NULL)
+
+orange <- data.frame(Orange) %>% 
+  group_by(Tree) %>% 
+  mutate("Sample" = as.numeric(seq(1:7))) %>% 
+  ungroup() 
+
+  pivot_wider(names_from = "Sample", values_from = c(age, circumference))
+
+ggplot(orange, aes(circumference, age, color= Tree, size=circumference)) +
+  geom_point() +
+  scale_fill_brewer(palette = "Set3") +
+  scale_size(range = c(2, 12)) +
+  scale_x_log10() +
+  transition_time(Sample) +
+  labs(title = 'Size by Age', x = 'Age in Days', y = 'Circumference in mm')
+orange_plot
+
+orange_gif <- animate(orange_plot, renderer = gifski_renderer())
+anim_save('orange_gif', animation = last_animation(), path = NULL)
