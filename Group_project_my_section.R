5672066f5eff077c7b4ba0cb45f4345caa99d82b
shelbywilliford <sbwilliford@uncg.edu>
2023-04-17 15:34:57 -0400

    just trying to change my branch

--- Group_project_my_section.R
+++ Group_project_my_section.R
@@ -1,50 +1,29 @@
+# For part  
+
 library(tidyverse)
 library(gganimate)
 library(gifski)
 library(RColorBrewer)
-
-mtcars
-p <- ggplot(mtcars, aes(factor(cyl), mpg)) + 
-  geom_boxplot() + 
-  transition_states(
-    gear,
-    transition_length = 2,
-    state_length = 1
-  ) +
-  enter_fade() + 
-  exit_shrink() +
-  ease_aes('sine-in-out')
-pq <- animate(p, renderer = gifski_renderer())
-anim_save('filename.gif', animation = last_animation(), path = NULL) 
-
-tic <- ggplot(titanic, aes(factor(Pclass), Survived)) + 
-  geom_boxplot() + 
-  transition_states(
-    gear,
-    transition_length = 2,
-    state_length = 1
-  ) +
-  enter_fade() + 
-  exit_shrink() +
-  ease_aes('sine-in-out')
-tic1 <- animate(tic, renderer = gifski_renderer())
-anim_save('tic.gif', animation = last_animation(), path = NULL)
 
 orange <- data.frame(Orange) %>% 
   group_by(Tree) %>% 
   mutate("Sample" = as.numeric(seq(1:7))) %>% 
   ungroup() 
 
-  pivot_wider(names_from = "Sample", values_from = c(age, circumference))
-
-ggplot(orange, aes(circumference, age, color= Tree, size=circumference)) +
+ggplot(orange, aes(age, circumference, color= Tree, size=circumference)) +
   geom_point() +
   scale_fill_brewer(palette = "Set3") +
-  scale_size(range = c(2, 12)) +
-  scale_x_log10() +
   transition_time(Sample) +
-  labs(title = 'Size by Age', x = 'Age in Days', y = 'Circumference in mm')
+  labs(title = 'Orange Tree Circumference by Age', x = 'Age in Days', y = 'Circumference in mm')
 orange_plot
 
 orange_gif <- animate(orange_plot, renderer = gifski_renderer())
 anim_save('orange_gif', animation = last_animation(), path = NULL)
+
+ggplot(orange, aes(age, circumference)) +
+  geom_col() +
+  scale_fill_brewer(palette = "Set3") +
+  facet_wrap("Tree") +
+  transition_states(Sample) +
+  shadow_mark() 
+  
