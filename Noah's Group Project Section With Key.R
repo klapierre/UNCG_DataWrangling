612f2854d111f17891e6571fec9a1383978c3754
Noah-nrhollan <123416196+Noah-nrhollan@users.noreply.github.com>
2023-04-21 16:13:53 -0400

    Filled out more on tidying data and the final graph tasks

--- Noah's Group Project Section With Key.R
+++ Noah's Group Project Section With Key.R
@@ -4,7 +4,10 @@
 
 
 # Now that you've had some practice with gganimate, it's time to really test your knowledge!
-# 
+
+# In this section, we're going to use the power of gganimate to better display 
+# how different factors affected who would survive the sinking of the Titanic
+
 # But first, lets get some new data to mess around with.
 # Make sure you have the "titanic.csv" file downloaded from the github branch or Canvas.
 # Then load it into R as an object and name it "rawTitanic"
@@ -24,7 +27,7 @@ rawTitanic <- read.csv("titanic.csv")
 ####+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
 # Answer: something along the lines of how you can't separate number of siblings 
 # from spouses or parents from children. Or, any other problems that make sense
-###++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+####+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
 
 # Question: What columns seem to be missing information?
 
@@ -34,18 +37,62 @@ rawTitanic <- read.csv("titanic.csv")
 
 
 # Now, as we can see, this data is pretty untidy with lots of empty cells and
-# columns that are tricky to work with. 
+# columns that are tricky to work with. So lets trim it down to just what we want
 
 # Task: Make a new dataframe called "titanicTidyAge" and have it only include the
 # PassengerId, Survived, Pclass, Name, Sex, and Age columns.
-# Then filter it to remove any N/A values in any of the cells. Try using the 
-# na.omit() function
+# Filter out any N/A values in any of the cells. Try using the na.omit() function.
+# Then make a new column with the age as a 10 year range or age in decades
 
 ####+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
 # Answer: 
 titanicTidyAge <- rawTitanic %>% 
   select(c(PassengerId, Survived, Pclass, Name, Sex, Age)) %>% 
+  na.omit() %>% 
+  mutate(AgeDecade = floor(Age/10)*10)
+
+####+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+
+
+# Once you have that we'll make a second data frame named "titanicTidyCabin" 
+# 1) Include the PassengerId, Survived, Pclass, Sex, Age, and Cabin columns
+# 2) Make a new column that includes just the starting letter from the cabin number, and an N/A if the cell is empty. (try out grepl() functions for this)
+# This letter is the deck the cabin was located, A was the topmost deck and B the
+# next, and so on
+
+####+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+# Answer: or anything that makes the same table like some complicate ifelse setup
+titanicTidyCabin <- rawTitanic %>% 
+  select(c(PassengerId, Survived, Pclass, Sex, Age, Cabin)) %>% 
+  mutate(Deck = str_extract(Cabin, "[aA-zZ]+")) %>% 
   na.omit()
 
-###++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+####+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+
+
+# Next you'll be using each of those to make a graph.
+
+# Task: Make a bar plot using the titanicTidyAge data frame that:
+# 1) Plots Sex against number of survivors 
+# 2) Animate the length of the bars by decade age groups
+# 3) Be sure to add appropriate x- and y-axes labels and title
+
+####+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+# Answer: 
+
+
+####+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+
+
+# Task: Make a scatter plot using the titanicTidyCabin data frame:
+# 1) Plotting Percentage of female passengers that survived against percentage of
+# male passengers who survived
+# 2) Coloring points by Pclass
+# 3) Animating the point position by Deck, from highest deck to lowest
+
+####+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+# Answer: 
+
+
+####+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
 
