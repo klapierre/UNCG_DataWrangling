923e2de4b5e90051898e394af101b6f020627ca6
ksvaarre <kaysasvl@gmail.com>
2023-04-24 19:24:53 -0400

    finished

--- gganimate.R
+++ gganimate.R
@@ -348,8 +348,8 @@ ggplot(orange, aes(circumference, group = Tree)) +
 # But first, lets get some new data to mess around with.
 # Make sure you have the "titanic.csv" file downloaded from the github branch or Canvas.
 # Then load it into R as an object and name it "rawTitanic"
-
-
+library(readr)
+rawTitanic <- read_csv("titanic.csv")
 
 # This data is a passenger list for the RMS Titanic, including information on the
 # different passengers' names, if they survived, sex, age, number of siblings/spouses aboard, number of parents/children aboard, ticket number, ticket fare,
@@ -360,11 +360,11 @@ ggplot(orange, aes(circumference, group = Tree)) +
 
 # Question: What problems could the SibSp and Parch columns make for us due to 
 # how they are recorded?
-
+## you can't tell how many of the recorded value are siblings vs. spouses, or parents vs. children
 
 
 # Question: What columns seem to be missing information?
-
+## cabin number and age
 
 # Now, as we can see, this data is pretty untidy with lots of empty cells and
 # columns that are tricky to work with. So lets trim it down to just what we want
@@ -374,7 +374,13 @@ ggplot(orange, aes(circumference, group = Tree)) +
 # Filter out any N/A values in any of the cells. Try using the na.omit() function.
 # Then make a new column with the age as a 10 year range or age in decades
 # (Make sure the highest age is "60+")
-
+titanicTidyAge <- rawTitanic %>% 
+  select(PassengerId, Survived, Pclass, Name, Sex, Age) %>% 
+  na.omit() %>%
+  mutate(AgeDecade = floor(Age/10)*10) %>% 
+  mutate(AgeDecade = ifelse(AgeDecade>=60,
+                            "60+",
+                            AgeDecade))
 
 
 # Once you have that we'll make a second data frame named "titanicTidyCabin" 
@@ -382,7 +388,11 @@ ggplot(orange, aes(circumference, group = Tree)) +
 # 2) Make a new column that includes just the starting letter from the cabin number, and an N/A if the cell is empty. (try out grepl() functions for this)
 # This letter is the deck the cabin was located, A was the topmost deck and B the
 # next, and so on
-
+titanicTidyCabin <- rawTitanic %>% 
+  select(PassengerId, Survived, Pclass, Sex, Age, Cabin) %>% 
+  mutate(Deck = str_extract(Cabin, "[A-Z]+")) %>% 
+  na.omit()
+  
 
 
 # Next you'll be using each of those to make a graph.
@@ -394,16 +404,35 @@ ggplot(orange, aes(circumference, group = Tree)) +
 # 4) Be sure to add appropriate x- and y-axes labels and title
 # 5) Color the columns something other than the default
 # Reminder: You may need to further filter the data frame
+tidyAge <- titanicTidyAge %>% 
+  filter(Survived==1)
 
-
+ggplot(tidyAge, aes(y=Survived, x=Sex, fill = Sex)) +
+  geom_bar(stat="identity") +
+  transition_states(AgeDecade, state_length = 1, transition_length = 3) +
+  ease_aes("sine-in-out") +
+  labs(subtitle = "Passenger age: {closest_state}'s")+
+  enter_fade()+
+  exit_fade()+
+  scale_fill_brewer(palette = "Accent")
+  
 # Task: Make a scatter plot using the titanicTidyCabin data frame:
 # 1) Plotting age of passengers on the y-axes and deck level on the x-axes
 # 2) Coloring points by Survived, with point shape by Sex
 # 3) Animating the point position by Pclass
 # 4) Jitter the points so they don't all overlap
 # 5) Add code to display what Pclass you're currently showing
-# 6) Add appropriate x- and y-axes labels
-# 
+# 6) Add appropriate x- and y-axes labels 
+
+ggplot(titanicTidyCabin, aes(x=Deck, y=Age, color=Survived, shape=Sex))+
+  geom_point(aes(group = seq_along(Pclass)), position = "jitter") +
+  scale_shape_manual(values = c(21, 24)) +
+  scale_fill_manual(values = c("white", "black"),guide = guide_legend(override.aes = list(shape = 21))) +
+  transition_states(Pclass, transition_length = 1, state_length = 1) +
+  enter_fade() +
+  exit_fade() +
+  labs(subtitle = "Class {closest_state} passengers")
+
 # Hint: Try aes(group = seq_along(...)) to get all your points to disappear 
 # between frames rather than some disappearing and some moving
 # see https://cran.r-project.org/web/packages/gganimate/vignettes/gganimate.html
