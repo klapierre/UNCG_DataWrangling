06471979587d5db3124c975596d1d66bd5e968f4
klapierre <kjkomatsu@uncg.edu>
2026-01-26 16:00:14 -0500

    Updating to run swirl module in week 3.

---
+++ 2_swirl_altSched.R
@@ -0,0 +1,199 @@
+# ---------------------------------------------------------- #
+#### MODULE 1: Practice with R in swirl                   ####               
+# ---------------------------------------------------------- #
+
+## OBJECTIVE:
+## To explore the basic building blocks of the R programming language.
+## To become comfortable running and writing R code and working in the R Studio environment.
+
+
+# ---------------------------------------------------------- #
+#### PART 1: SWIRL COURSES AND ASSOCIATED QUESTIONS       ####
+# ---------------------------------------------------------- #
+
+# INSTRUCTIONS:
+# Take the following courses in swirl.
+# All below are all modules in the R Programming course
+
+# Some of these will be parallel to and reinforcing previous in-class exercises, 
+# and others will expand on what we have learned. 
+# As you proceed with each module please pause and use the play() function 
+# at relevant stages in each module to answer the associated questions below. 
+# When you are ready to start working through the swirl() module again
+# just type nxt().
+# Please note that swirl is a little buggy, and if you answer a multiple choice 
+# incorrectly it may give you an error and exit swirl. 
+# If that happens, type swirl() to resume and enter the same user name you used 
+# previously, and you should be able to navigate directly back to where 
+# you left off. 
+
+# If swirl is not yet installed, install it first
+install.packages("swirl")
+# Then load swirl
+library(swirl)
+# Then install the course R Programming
+install_course("R Programming")
+
+# Now run the code below
+swirl()
+# You should now be able to navigate to the R Programming Course and the specific
+# modules below. Complete modules 1-8 and 15, pausing between each module to 
+# answer the questions below. Then continue on to Part II, testing out your 
+# ability to put what you learned into practice. Finish with Part III to provide
+# feedback on what you think of swirl.
+
+# ---------------------------------------------------------- #
+#### I. R Programming -> 1. Basic Building Blocks         #### 
+# ---------------------------------------------------------- #
+# QUESTION: What would you do to get back a line of code you previously ran
+# from your console without retyping the whole thing again?
+
+
+# ---------------------------------------------------------- #
+#### II. R Programming -> 2. Workspace and Files          #### 
+# ---------------------------------------------------------- #
+# QUESTION: What is the difference between your working directory
+# and your local workspace?
+
+# QUESTION: How do you find help files for a function using R code?
+
+
+# ---------------------------------------------------------- #
+#### III. R Programming -> 3. Sequences of Numbers        #### 
+# ---------------------------------------------------------- #
+# QUESTION: In R, is there usually just one way or many different
+# ways of doing the same thing? Why might this be useful?
+
+
+# ---------------------------------------------------------- #
+#### IV. R Programming -> 4. Vectors                      #### 
+# ---------------------------------------------------------- #
+# QUESTION: Describe in your own words the difference between a 
+# logical vector, a character vector, an integer vector, and a 
+# numeric vector.
+
+# QUESTION: What does <= stand for?
+
+# QUESTION: What does == stand for?
+
+# QUESTION: What does != stand for?
+
+# QUESTION: What does | mean in R?
+
+# QUESTION: What does & mean when testing for TRUE/FALSE statements?
+
+
+# ---------------------------------------------------------- #
+#### V. R Programming -> 5. Missing Values                ####  
+# ---------------------------------------------------------- #
+
+# TASK: Copy your assignment of my_data to the script below, then create a new 
+# object called my_data2 using the same procedure.  
+# Using the R function identical(), test whether my_data and my_data2 
+# are identical.
+# Hint: You can always Google or in R type ?identical() to get
+# help on using this (or any) function.
+
+
+# QUESTION: Explain why my_data and my_data2 were or were not identical.
+
+
+# ---------------------------------------------------------- #
+#### VI. R Programming -> 6. Subsetting                   ####
+# ---------------------------------------------------------- #
+# QUESTION: Subsetting can be an amazing thing! Describe one real world
+# scenario where you might want to subset a dataset. (you can make up anything)
+
+# QUESTION: Sometimes R gives you an answer to a question that shouldn't be
+# asked (e.g., when you asked for the 3000th variable in your vector x). Why
+# might this be an issue?
+
+
+# ---------------------------------------------------------- #
+#### VII. R Programming -> 7. Matrices and Data Frames    ####
+# ---------------------------------------------------------- #
+# QUESTION: In the code matrix[x,y], which letter represents the rows? 
+# Which letter represents the columns?
+
+
+# QUESTION: What are two different ways you could assign the numbers 1-20
+# into a matrix with 4 rows and 5 columns?
+
+
+# ---------------------------------------------------------- #
+#### VIII. R Programming -> 8. Logic                      ####
+# ---------------------------------------------------------- #
+# TASK: Leave a comment that you have completed this section. 
+# Did you feel like you were taking an LSAT?
+
+
+# ---------------------------------------------------------- #
+#### VII. R Programming -> 15. Base Graphics              ####
+# ---------------------------------------------------------- #
+
+# TASK: dim(), names(), head(), and tail() are all great functions!
+# Run each of them on the cars dataset.
+
+# QUESTION: In your own words, describe what each of these functions do and
+# why each one might be useful.
+
+# TASK: Run the str() function on the cars dataset.
+
+# QUESTION: In your own words, describe what the str() function does.
+# Why might this be useful?
+
+
+# ---------------------------------------------------------- #
+#### PART 2: TEST WHAT YOU LEARNED                        ####
+# ---------------------------------------------------------- #
+
+# I) Without running the code, what does the following block 
+# of code print? Please explain why.
+a <- 1
+b <- 2
+c <- a + b
+b <- 4
+a <- b
+c <- a
+c
+
+
+# II) Three of the following lines produce the same result. 
+# Without running the code, which one will produce a different result than the
+# others? Please explain why. 
+# HINT: The helpfile for the log function may be useful. 
+log(x = 1000, base = 10)
+log10(1000)
+log(base = 10, x = 1000)
+log(10, 1000)
+
+
+
+# III) Use one of R's built in functions to create a new vector that is 
+# the entries in numvector arranged in descending order. We have not learned 
+# this function, but a combination of Google and function documentation 
+# should get you there.
+numvector <- c(5,2,3,1,6,8)
+
+
+# IV) Which elephant weighs more? 
+# Convert one’s weight to the units of the other, and store the result in an 
+# appropriately-named new variable. Test whether elephant1 weights more than 
+# elephant2 using an equation that returns either true or false (1 kg ≈ 2.2 lb).
+
+elephant1_kg <- 3492
+elephant2_lb <- 7757
+
+
+# ---------------------------------------------------------- #
+#### PART 3: FEEDBACK                                     ####
+# ---------------------------------------------------------- #
+# QUESTION: What did you think of the Swirl tutorial? Was it helpful? Why 
+# or why not?
+
+# QUESTION: Would you have preferred to split the Swirl tutorial over weeks
+# 1 and 3 of the class instead of the week 1 assignment you previously 
+# completed? Why or why not?
+
+# REMEMBER: Save and upload your script to Canvas when you're done with
+# this assignment!
