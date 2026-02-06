7db9b3aa3a0b5802f27cf09f02aba469b3939658
klapierre <kjkomatsu@uncg.edu>
2026-02-06 17:18:25 -0500

    Updating intro instructions.

--- 4_wrangle.R
+++ 4_wrangle.R
@@ -3,26 +3,31 @@
 # ---------------------------------------------------------- #
 
 ## OBJECTIVE:
-# 1. To learn how to manipulate and transform data into a form usable for analysis and graphs.
+# 1. To learn how to manipulate and transform data into a form usable for 
+# analysis and graphs.
 # 2. To do this in a way that each step is traceable and reproducible.
-# To this end we'll be familiarizing ourselves with and diving into the dplyr package.
+# To this end we'll be familiarizing ourselves with and diving into the dplyr 
+# package.
 
 
 # ---------------------------------------------------------- #
 #### SET UP:                                              ####
 # ---------------------------------------------------------- #
-# REMINDER: Create a new branch for this week through the GitHub website
-# called yourLastName_week4. Open the Rproject for our course
-# (UNCG_DataWrangling),pull any changes from GitHub, and navigate to the
-# branch you created for this week in RStudio's Git tab. Open this week's
-# assignment in this new branch to complete and submit on GitHub.
-# Ideally, you'll commit your answers a bit at a time as you work through
-# this assignment, rather than committing all at once at the end.
+# REMINDER: Be sure you are working in YOUR branch of our repository for all 
+# commits related to this assignment. Open the Rproject for our course
+# (UNCG_DataWrangling), pull any changes from GitHub, and navigate to the
+# branch you created RStudio's Git tab. Open this week's
+# assignment in your branch to complete and submit on GitHub.
+# Ideally, you'll commit your answers a bit at a time (like you were prompted to
+# do last week) as you work through this assignment, rather than committing all 
+# at once at the end.
 
 
-# The dplyr package is nested within the tidyverse package (along with many others).
-# Install tidyverse and load the library. HINT: see the end of assignment #1 if you 
-# forgot how to install and load a package.
+# The dplyr package is nested within the tidyverse package (along with many 
+# others).
+
+# TASK: Install tidyverse and load the library. HINT: see the end of assignment 
+# #1 if you forget how to install and load a package.
 
 
 # ---------------------------------------------------------- #
