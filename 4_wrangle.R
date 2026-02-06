135a31a2536a5cd8967d7cc7e55f41a3e311816f
klapierre <kjkomatsu@uncg.edu>
2026-02-06 17:32:46 -0500

    Updating instructions to include rebasing from main in case of future main changes.

--- 4_wrangle.R
+++ 4_wrangle.R
@@ -14,10 +14,15 @@
 #### SET UP:                                              ####
 # ---------------------------------------------------------- #
 # REMINDER: Be sure you are working in YOUR branch of our repository for all 
-# commits related to this assignment. Open the Rproject for our course
-# (UNCG_DataWrangling), pull any changes from GitHub, and navigate to the
-# branch you created RStudio's Git tab. Open this week's
-# assignment in your branch to complete and submit on GitHub.
+# commits related to this assignment.
+# (1) Open the Rproject for our course (UNCG_DataWrangling), navigate to the 
+# 'main' branch, and pull any changes from GitHub.
+# (2) Navigate to the branch YOU created in RStudio's Git tab.
+# (3) Only once you are in your branch, go to the terminal (tab next to console),
+# and run the following code:
+# git rebase main
+# (4) Open this week's assignment in your branch to complete and submit on GitHub.
+
 # Ideally, you'll commit your answers a bit at a time (like you were prompted to
 # do last week) as you work through this assignment, rather than committing all 
 # at once at the end.
@@ -26,8 +31,8 @@
 # The dplyr package is nested within the tidyverse package (along with many 
 # others).
 
-# TASK: Install tidyverse and load the library. HINT: see the end of assignment 
-# #1 if you forget how to install and load a package.
+# TASK: Install tidyverse and load the library. HINT: see the end of the week 1 
+# assignment if you forget how to install and load a package.
 
 
 # ---------------------------------------------------------- #
