fe2e0a172dfa5456f3ec8b7b41549d6397f25859
katadams1204-star <katadams1204@gmail.com>
2026-02-09 22:50:13 -0500

    Sorted the duplicates dataframe to highlight a pattern in the rows.

--- 3_openScience_altSched.R
+++ 3_openScience_altSched.R
@@ -183,6 +183,7 @@ duplicates[with(duplicates, order(RecDate, Block, Plot, Taxa)),]
 ## QUESTION: What do you notice about the data? Specifically, compare rows 
 ## 4934 and 12173 (if they are sorted correctly, those should be on top).
 
+#They are two versions of the same row. Every row has a duplicate of itself.
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to the branch
 ## you created for this week with an appropriate commit message.
