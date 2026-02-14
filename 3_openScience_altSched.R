c5be30d0b281b51d0e7acfa55549f75bb57231ea
dylanmarlowe <dylanmarlowe03@gmail.com>
2026-02-14 13:45:00 -0500

    I'm currently working through the 5th objective. I created a data frame labelled "duplicates" using rbind() with both "conSME" data frames. This totals the observations of both data frames into one data frame (9245+9245 = 18490).

--- 3_openScience_altSched.R
+++ 3_openScience_altSched.R
@@ -197,10 +197,19 @@ colnames(conSMEcoverAlt)
 ## rbind() function.
 ## (Hint: Very similar to the cbind function we used in the Week 1 assignment)
 
+duplicates <- rbind(conSME_abundance,conSMEcoverAlt)
+
+
 
 ## QUESTION: Looking at the information for each dataframe in the environment tab
 ## of RStudio, what do you notice about the number of observations for the 
 ## duplicates dataframe compared to the two original dataframes?
+
+## The number of observations in the data frame "duplicates" is twice the number of observations in each of the other "conSME" data frames. This means that the "duplicates" data frame correctly bound the number observations of both "conSME" data frames into one large data frame. 
+
+9245+9245
+[1] 18490
+
 
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to 
