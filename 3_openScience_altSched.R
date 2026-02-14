430753324dd3a6fe57e6bd68dc698d49972e6324
mariiiology <mchuff2@uncg.edu>
2026-02-14 15:25:55 -0500

    helped you find information quickly

--- 3_openScience_altSched.R
+++ 3_openScience_altSched.R
@@ -178,7 +178,7 @@ duplicates[with(duplicates, order(RecDate, Block, Plot, Taxa)),]
 
 ## QUESTION: What do you notice about the data? Specifically, compare rows 
 ## 4934 and 12173 (if they are sorted correctly, those should be on top).
-
+## They have the same information listed twice 
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to the branch
 ## you created for this week with an appropriate commit message.
