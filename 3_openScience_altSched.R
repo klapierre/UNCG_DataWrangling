574c222857054711282def12a88dabdec2f90408
amaliyabrown <amaliya@ad.unc.edu>
2026-02-15 10:14:22 -0500

    learning to amend

--- 3_openScience_altSched.R
+++ 3_openScience_altSched.R
@@ -215,14 +215,14 @@ noDuplicates <- unique(duplicates)
 # ----------------------------------------------------------
 
 ## TASK: Type a comment below.
-
+## Learning Github.
 
 ## TASK: Follow these instructions carefully!
 ## Save your R script. Stage and commit with the commit message "learning to 
 ## amend", but this time DON'T pull/push your modified code!
 
 ## TASK: Type another comment below.
-
+## Ammending in R.
 
 ## TASK: Save your R script. Open up the commit window in RStudio.
 
