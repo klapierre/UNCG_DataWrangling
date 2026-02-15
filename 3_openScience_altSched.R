742159ffd0723c93b07690c3efe79d9fa35592b6
thearnavpandey <a_pandey2@uncg.edu>
2026-02-15 16:26:45 -0500

    completed till mid-part 5

--- 3_openScience_altSched.R
+++ 3_openScience_altSched.R
@@ -191,7 +191,7 @@ duplicates[with(duplicates, order(RecDate, Block, Plot, Taxa)),]
 
 ## QUESTION: What do you notice about the data? Specifically, compare rows 
 ## 4934 and 12173 (if they are sorted correctly, those should be on top).
-
+# They were not on top.They had different info for RecDate, Block, Plot, Taxa.
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to the branch
 ## you created for this week with an appropriate commit message.
