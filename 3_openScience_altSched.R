c99a184f1bcbfeceea30aa334e9d4b845336474d
amaliyabrown <amaliya@ad.unc.edu>
2026-02-15 09:41:19 -0500

    made a dataframe called "duplicates" that binds conSME_abundance and conSMEcoverAlt.

--- 3_openScience_altSched.R
+++ 3_openScience_altSched.R
@@ -136,7 +136,7 @@ colnames(conSMEcoverAlt)
 
 ## QUESTION: Do all of your objects follow the same naming convention or do you
 ## plan to use different naming conventions to reference different object types?
-
+##Answer: I plan to use the lowercase_underscore naming convention for most objects, but I plan to use distinct capitalization for dataframes to help distinguish them. 
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to 
 ## the branch you created for this week with an appropriate commit message.
@@ -155,7 +155,7 @@ colnames(conSMEcoverAlt)
 ## conSME dataframes you have created into one ultimate dataframe using the 
 ## rbind() function.
 ## (Hint: Very similar to the cbind function we used in the Week 1 assignment)
-
+duplicates <- rbind(conSME_abundance, conSMEcoverAlt)
 
 ## QUESTION: Looking at the information for each dataframe in the environment tab
 ## of RStudio, what do you notice about the number of observations for the 
