38e396ab4a906f1b3c0a94511513edb04f8dc1a0
nsaltsgaver <nsaltsgaver1@gmail.com>
2026-02-16 15:14:55 -0500

    Data sorted for duplicates.

--- 3_openScience_altSched.R
+++ 3_openScience_altSched.R
@@ -170,7 +170,7 @@ duplicates[with(duplicates, order(RecDate, Block, Plot, Taxa)),]
 
 ## QUESTION: What do you notice about the data? Specifically, compare rows 
 ## 4934 and 12173 (if they are sorted correctly, those should be on top).
-
+## They both contain the same data for each category.
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to the branch
 ## you created for this week with an appropriate commit message.
