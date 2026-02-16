760586e72511559e7af18e3be6d173fcf743affe
pattiekevrette <pkeverette@uncg.edu>
2026-02-16 13:53:02 -0500

    Assignment completed

--- 3_openScience_altSched.R
+++ 3_openScience_altSched.R
@@ -239,7 +239,7 @@ noDuplicates <- unique(duplicates)
 ## QUESTION: How many commits are you ahead of your branch on GitHub?
 ## (Hint: look for the message "Your branch is ahead of..." near the top of the 
 ## window.)
-#2
+#2 and I committed without amending and I went back and amended the second comment
 
 ## TASK: Stage your modified code and check the "Amend previous commit" box.
 ## Then commit your code (still don't pull/push).
@@ -247,7 +247,7 @@ noDuplicates <- unique(duplicates)
 
 ## QUESTION: What happened when you clicked "Amend previous commit"?
 ## How many commits is your branch ahead by now?
-
+#3 
 
 ## TASK: Once you've answered the above questions, save your R script one last time. 
 ## Stage your modified code, amend it to the previous commit, and finally pull/push
