6f90cad4e6fced8f6f30840763fb2a55cbae2ced
pattiekevrette <pkeverette@uncg.edu>
2026-02-16 13:25:45 -0500

    Sorted duplicates by Taxa, Plot, Block, Watershed and RecDate

--- 3_openScience_altSched.R
+++ 3_openScience_altSched.R
@@ -184,7 +184,7 @@ duplicates[with(duplicates, order(RecDate, Block, Plot, Taxa)),]
 
 ## QUESTION: What do you notice about the data? Specifically, compare rows 
 ## 4934 and 12173 (if they are sorted correctly, those should be on top).
-
+#Its sorting to 14179... and they are the same or duplicates
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to the branch
 ## you created for this week with an appropriate commit message.
