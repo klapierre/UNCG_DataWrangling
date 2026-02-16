70c7f4e6639828bd6217ef87ae8979f8339504c5
hazelrosemarie <bigsisterhrmm@gmail.com>
2026-02-16 16:06:44 -0500

    Script is completed through part 1.4

--- 4_wrangle.R
+++ 4_wrangle.R
@@ -315,17 +315,20 @@ calispellTempSum <- mutate(.data=calispellTempF,
                            sum=calispell_temp + calispell_temp_F)
 
 # Check the dataframe to see if it worked.
-
+head(calispellTempSum)
 
 # TASK: The column we just created makes no sense (why would you ever want to
 # sum the C and F temperatures?). Write code below to remove it from the dataframe.
-
+calispellTempMinus <- select(.data=calispellTempSum,
+                         -sum)
+head(calispellTempMinus)
 
 # QUESTION: We might also want to add a column that describes the dataset. What 
 # happens when you run the following code?
 calispellTempFaquatic <- mutate(.data=calispellTempF,
                                 type='aquatic')
-
+head(calispellTempFaquatic)
+#Answer: Now there is another column called type, and every observation is labeled as aquatic in that column.
 
 # ---------------------------------------------------------- #
 ### PART 1.5: PASTING AND SEPARATING COLUMNS              ####
