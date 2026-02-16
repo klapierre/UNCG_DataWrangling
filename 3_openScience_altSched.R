80c87b5821997cb8b6f12cd0bf47613ce06a4ef4
kmclark4 <kmclark4@uncg.edu>
2026-02-16 02:05:43 -0500

    Clark_open-science_part5.2
    checking rows in the duplicates df.

--- 3_openScience_altSched.R
+++ 3_openScience_altSched.R
@@ -193,7 +193,10 @@ duplicates[with(duplicates, order(RecDate, Block, Plot, Taxa)),]
 
 ## QUESTION: What do you notice about the data? Specifically, compare rows 
 ## 4934 and 12173 (if they are sorted correctly, those should be on top).
+duplicates[4934, ]
+duplicates[12173, ]
 
+#ANSWER: They are from different years, watersheds, blocks, plots, and are different spp. They are not duplicated rows. 
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to the branch
 ## you created for this week with an appropriate commit message.
