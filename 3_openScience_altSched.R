5c55c180f335622beef333739ca35fd6e95889f2
Michu-leyva <tiaramaya15606@gmail.com>
2026-02-16 02:40:28 -0500

    combined conSME_abundance and conSMEcoverAlt into data frame named duplicates using rbind.

--- 3_openScience_altSched.R
+++ 3_openScience_altSched.R
@@ -159,12 +159,13 @@ colnames(conSMEcoverAlt)
 ## conSME dataframes you have created into one ultimate dataframe using the 
 ## rbind() function.
 ## (Hint: Very similar to the cbind function we used in the Week 1 assignment)
-
+duplicates <-rbind(conSME_abundance, conSMEcoverAlt)
 
 ## QUESTION: Looking at the information for each dataframe in the environment tab
 ## of RStudio, what do you notice about the number of observations for the 
 ## duplicates dataframe compared to the two original dataframes?
-
+# there are 18490 observations, in the abundance and alt data frames there are only 9245 
+# observations.   
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to 
 ## the branch you created for this week with an appropriate commit message.
