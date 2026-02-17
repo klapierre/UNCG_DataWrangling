db26590a02b6f6f5581cfd878284e80c727988f0
smaldridge0211 <smaldridge@uncg.edu>
2026-02-16 19:59:28 -0500

    Added answers to lines between 101-131.

--- 3_openScience_altSched.R
+++ 3_openScience_altSched.R
@@ -107,23 +107,23 @@ conSMEcoverAlt <- read.csv("https://pasta.lternet.edu/package/data/eml/knb-lter-
 
 ## TASK: Check the names of the columns in the species cover dataset you imported.
 ## (Hint: remember the Week 1 assignment?)
-
+#used colnames function
 
 ## QUESTION: What naming convention did the dataset creators use for column names?
-
+#They used capitalization to distinguish between words.They did not use any punctuation.
 
 ## QUESTION: What naming convention do you plan to use for this course for the
 ## following types of objects in R:
-## R scripts
-## vectors
-## dataframes
-## columns within dataframes
-## homemade functions
+## R scripts: using underscores
+## vectors: using dashes to separate words
+## dataframes: using capitalization to differentiate between words
+## columns within dataframes: all caps
+## homemade functions: double dash
 
 
 ## QUESTION: Do all of your objects follow the same naming convention or do you
 ## plan to use different naming conventions to reference different object types?
-
+# I plan to use different naming conventions for each type of object
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to 
 ## the branch you created for this week with an appropriate commit message.
