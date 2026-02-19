97c97fc8fae8e4280dcf98287793319d922674b9
katadams1204-star <katadams1204@gmail.com>
2026-02-19 12:06:41 -0500

    Completed assignment.

--- 4_wrangle.R
+++ 4_wrangle.R
@@ -432,21 +432,34 @@ calispellHighTemp <- read.csv("CalispellCreekandTributaryTemperatures.csv", stri
 # TASK: Perform the following steps in one workflow (i.e., using pipes):
 # (1) Create a dataframe called cdr and load the .csv file 
 # 'e001_Plant aboveground biomass carbon and nitrogen.csv' into it.
+cdr <- read.csv("e001_Plant aboveground biomass carbon and nitrogen.csv", stringsAsFactors = TRUE)
 # (2) Add a column named 'Exp' and fill it with the text 'e001' so we know what 
 # experiment we're working with.
+cdr <- mutate(cdr,
+              Exp="e001") %>% 
 # (3) Rename our last two columns that were originally '% Carbon' and '% Nitrogen' 
 # in our csv file. Make the new names 'C' and 'N', respectively.
+rename(C=X..Carbon,
+       N=X..Nitrogen) %>% 
 # (4) Remove any observations that were not obtained from Strip 1.
+filter(Strip==1) %>% 
 # (5) Create a new column called NTrtInfo that include the information from both 
 # NTrt and NAdd, separated by an underscore.
+  unite(col='NTrtInfo',
+        c('NTrt', 'NAdd'),
+        sep='_') %>% 
 # (6) Split the Species column into two columns, one named 'genus' and one named 
 # 'species'.
+separate(col=Species,
+           into=c('genus', 'species'),
+           sep=' ') %>% 
 # (7) Create a column called CN that contains the ratio of C to N for each 
 # observation.
+mutate(CN = C/N) %>% 
 # HINT: The ratio of C to N is calculated as C/N.
 # (8) Keep only the following columns: Exp, Date, Plot, NTrtInfo, genus, species, 
 # Field, C, N, and CN. 
-
+select(Exp, Date, Plot, NTrtInfo, genus, species, Field, C, N, CN)
 
 
 # REMEMBER: Save and push your script to your branch when you're done with this 
