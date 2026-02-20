0547d04b2f24937e0345aee3fb6f6ccdc222eb88
bndixon54 <baileydixon54@gmail.com>
2026-02-20 15:53:28 -0500

    notes

---
+++ notes_week7.R
@@ -0,0 +1,13 @@
+# select() function allows subsetting by columns
+# filter() function allows subsetting by rows
+# arrange() function to rearrange rows
+# mutate() function to make a new variable in dataframe
+    # unite() to combine to text variables together
+    # separate() to split columns apart
+
+# pseudocode is the planning stage; there should be a line for each part of the data cleaning phase 
+
+
+
+         
+         
