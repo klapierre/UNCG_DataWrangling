8259c3010bee16fd234868f8362ebcce5f93ab35
pattiekevrette <pkeverette@uncg.edu>
2026-02-23 11:25:38 -0500

    1.3 completed on the Wrangle assignment

--- 4_wrangle.R
+++ 4_wrangle.R
@@ -233,6 +233,7 @@ identical(calispellTemp3, calispellTemp5)
 # QUESTION: If you remove all of the observations (rows) with temperatures lower
 # than 15 C, would you expect your new dataframe to have more, the same, or fewer
 # observations than the original dataframe?
+#fewer observations that the original dataframe.
 
 
 # TASK: Run the following code to only keep the values greater than or equal to 15C.
@@ -243,11 +244,12 @@ calispellHighTemp <- filter(.data=calispellTemp,
 # TASK: Check the number of observations in your dataframe! You can either do 
 # this using the str() function or by looking next to the dataframe name in the
 # R environment tab.
-
+#To knoe if it worked I checked the environment tab.
 
 # QUESTION: How many observations did the original dataframe (calispellTemp) 
 # have? How many does the new dataframe (calispellHighTemp) have?
-
+#The original had 61100 and the new has 7703 observations. This shows I am correct
+#that the new dataframe has fewer observations.
 
 # REALLY IMPORTANT: Even if the function runs, R can do all kinds of bad things 
 # if you've accidentally coded something incorrectly. It is always very, very 
@@ -290,9 +292,11 @@ calispellData <- filter(.data=calispellTemp,
 
 # QUESTION: How many observations are in the datafile calispellData? Write code
 # to determine how many values of calispell_temp were NA.
+#52330 observations are in the datafile calispellData. 
+#There are no NA values in the calispell_temp column
+sum(is.na(calispellData$calispell_temp))
 
-
-# ---------------------------------------------------------- #
+    # ---------------------------------------------------------- #
 ### PART 1.4: CREATING COLUMNS                            ####
 # ---------------------------------------------------------- #
 
