352fadffd4945042148c097b05240e6849c5952c
dhanush0012 <dhanush.devanand@gmail.com>
2026-02-23 18:16:04 -0500

    Creating with mutate

--- 4_wrangle.R
+++ 4_wrangle.R
@@ -336,15 +336,19 @@ calispellTempSum <- mutate(.data=calispellTempF,
 
 # Check the dataframe to see if it worked.
 
+head(calispellTempSum)
 
 # TASK: The column we just created makes no sense (why would you ever want to
 # sum the C and F temperatures?). Write code below to remove it from the dataframe.
 
+calispellTempSum <- select(.data = calispellTempSum, -sum)
 
 # QUESTION: We might also want to add a column that describes the dataset. What 
 # happens when you run the following code?
 calispellTempFaquatic <- mutate(.data=calispellTempF,
                                 type='aquatic')
+
+##Column "type" is added and every row says "aquatic".
 
 
 # ---------------------------------------------------------- #
