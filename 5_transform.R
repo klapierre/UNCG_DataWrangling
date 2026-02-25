c55e12bcf003c1d8ae268be9692663466c7bd0a9
amaliyabrown <amaliya@ad.unc.edu>
2026-02-25 10:59:48 -0500

    Completed part 2.5 of the assignment.

--- 5_transform.R
+++ 5_transform.R
@@ -434,6 +434,7 @@ willowClean5 <- willowClean4 %>%
 # and the other willowData.
 
 # QUESTION: What columns would go in each of our two relational databases?
+# ANSWER: 'plotInfo' will contain the columns relating to the conditions of the plot (block, plot, code, snow, n, temp), while 'willowData' will contain the columns relating to the willow saplings' conditions (block, plot, willow_ID, ht1, ht2, cnpy1, cnpy2, notes, status, year).
 
 # Let's do it! Run the following code:
 plotInfo <- willowClean5 %>%
@@ -449,6 +450,7 @@ willowData <- willowClean5 %>%
 # TASK: Write code to join these two dataframes back together into a new 
 # dataframe called willowDataTrt using the left_join() function.
 
+willowDataTrt <- left_join(plotInfo, willowData)
 
 # ON YOUR OWN: There are so many ways to join databases! Think through when you 
 # might want to use each type. We will practice more with joining data in the 
