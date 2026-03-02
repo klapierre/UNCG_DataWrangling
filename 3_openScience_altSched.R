dc3c181b57bf5933069d68187d2844cec2089321
jas-lynnn <jcdavenport@uncg.edu>
2026-03-02 00:51:27 -0500

    Create duplicates dataframe and remove exact duplicates

--- 3_openScience_altSched.R
+++ 3_openScience_altSched.R
@@ -149,11 +149,12 @@ names(conSMEcoverAlt)
 ## rbind() function.
 ## (Hint: Very similar to the cbind function we used in the Week 1 assignment)
 
+duplicates <- rbind(conSME_abundance, conSMEcoverAlt)
 
 ## QUESTION: Looking at the information for each dataframe in the environment tab
 ## of RStudio, what do you notice about the number of observations for the 
 ## duplicates dataframe compared to the two original dataframes?
-
+#the duplicates is a both the original dataframes added togetre still with 12 variables.
 
 ## TASK: Save your R script. Stage, commit, and pull/push your modified code to 
 ## the branch you created for this week with an appropriate commit message.
