06ab1f897486e6e12884d78434028ca6b9d46a5b
bndixon54 <baileydixon54@gmail.com>
2026-03-09 14:19:19 -0400

    group project

--- group_map_project.R
+++ group_map_project.R
@@ -1,1 +1,8 @@
 # test group project file
+
+# ---------------------------------------------------------- #
+## OBJECTIVES:
+# 1. Understand when and why we may want to use Rstudio to build maps
+# 2. To learn how to find and import environmental spatial data sets
+# 3. Use packages such as maps and ggplot to map spatial data 
+# 4. Understand that there are may different ways to map spatial data within RStudio framework
