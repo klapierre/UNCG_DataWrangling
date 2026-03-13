252f9bc1d5fd6acd75c2cfebba0b1c3367f26623
dylanmarlowe <dylanmarlowe03@gmail.com>
2026-03-13 15:12:20 -0400

    I learned how to use facets to compile multiple plots together across different parts of the data frame.

--- 6_plot.R
+++ 6_plot.R
@@ -679,6 +679,11 @@ ggplot(redband, aes(x = Length, y = Weight)) +
 # HINT: Check the help file for facet_wrap if you're unsure. Look under the 
 # Arguments section for scales.
 
+ggplot(redband, aes(x = Length)) +
+  geom_histogram() +
+  facet_wrap(~ ScaleAge, scales = "free_y")
+
+
 
 # ---------------------------------------------------------- #
 #### PART 1.11: SAVING YOUR GRAPHICS                      ####
