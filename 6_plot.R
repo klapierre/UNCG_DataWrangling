2c16050f88898fac83e8ec0387aa4abe0598e502
Michu-leyva <tiaramaya15606@gmail.com>
2026-03-15 19:27:03 -0400

    created a new png.

--- 6_plot.R
+++ 6_plot.R
@@ -626,12 +626,17 @@ ggplot(redband, aes(x= Length)) +
 ggsave("Redband_histogram_facet.png")
 
 # QUESTION: Where did this file show up? And what was the graph?
-
+# The file showed up in the git tab and in the current working directory. 
+# The graph is "Redband_histogram_facet", the one we created at the end of part 1.10. 
 
 # TASK: Investigate the ggsave() function through the help files. Then write
 # code to save the file at 600 dpi, 10 inch width and 8 inch height.
-
-
+help(ggsave)
+ggsave("Redband_histogram_facet.png", 
+       dpi = 600, 
+       width = 10, 
+       height = 8, 
+       units = "in")
 # NOTE: You can also save the graphics you make by exporting them from the plots
 # tab in RStudio. However, this can be less precise than specifying the graphic
 # attributes using the ggsave function.
