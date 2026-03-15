8d4dd494988b9434a80f19f3a7973cbc814d9beb
p-mesha <themeshaparker@gmail.com>
2026-03-14 23:04:35 -0400

    Finished 1.5

--- 6_plot.R
+++ 6_plot.R
@@ -269,6 +269,7 @@ ggplot(redband, aes(x = Length, y = Weight)) +
 # in the above graph for our statistical transformation fit?
 # HINT: What is the default model type for a dataframe of our size?
 
+#LOESS is used 
 
 # We also can specify a specific model to fit. Try running the following code to
 # specify a linear model:
@@ -280,6 +281,7 @@ ggplot(redband, aes(x = Length, y = Weight)) +
 # task. Using the geom_smooth help page, write code below to specify a linear
 # model using a method= statement instead of the formula= statement.
 
+ggplot(redband, aes(x = Length, y = Weight)) +geom_point() + geom_smooth(method = "lm")
 
 # A linear model does not seem like a good fit to our data. Try running the
 # following code to generate a quadratic model.
@@ -300,15 +302,19 @@ ggplot(redband, aes(x = as.factor(ScaleAge), y=Weight)) +
 # assignment.
 # HINT: It was in the very first part of the assignment.
 
+# we used geom_histogram()
 
-# TASK: Let's put this all together! Create a graph with the following:
+# TASK: Let's put this all together! Create a graph with the following
 # (1) redband dataframe,
 # (2) x-axis = Length, y-axis = Weight
 # (3) points colored by ScaleAge as a factor
 # (4) quadratic line that is black in color and size=2 (HINT: check ggplot
 #     cookbook to help figure out how to change line color and size).
 
+  ggplot(redband, aes(x = Length, y = Weight)) + geom_point(aes(color = as.factor(ScaleAge))) + 
+  geom_smooth(formula = y ~ poly(x,2), color = "black", linewidth = 2)
 
+  
 # ---------------------------------------------------------- #
 #### PART 1.6 DATA IN VS DATA OUT                         #### 
 # ---------------------------------------------------------- #
