6ce3ff2f17f1d960b285395475d8083ec38367ab
smaldridge0211 <smaldridge@uncg.edu>
2026-03-16 15:32:17 -0400

    Added install package instructions. SA

--- Group 2 Project - stringr.R
+++ Group 2 Project - stringr.R
@@ -1,3 +1,11 @@
 ## Group 2 (stringr) Project Test
 
 # Test
+#Begin by installing the babynames packages using the code below.
+install.packages("babynames")
+
+#Then load the library.
+library(babynames)
+
+#Save the data to its own dataset called babyNames.
+babyNames <- babynames
