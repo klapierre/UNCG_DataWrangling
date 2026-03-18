e4944aeb10c1b5e9b7f602eaf70cbb5068a1558c
katadams1204-star <katadams1204@gmail.com>
2026-03-18 14:50:34 -0400

    Wrote instructions for understanding the str_pad function.

--- Group 2 Project - stringr.R
+++ Group 2 Project - stringr.R
@@ -25,10 +25,25 @@ strlengthtest <- c("This", "is", "to", "test", "stringr", "length", "functions!"
 #you need help, check the help file for this function.
 
 #The str_length function is useful if we only want to see strings of a certain 
-#character count. It can be combined with the filter function to accomplish this.
-#Suppose we want to pick a baby name, but only want names of a specific length.
-#For example, let's say we want a name with 6 or more characters.
+#character count. It can be combined with the filter function to accomplish this
+#in a dataframe.Suppose we want to pick a baby name, but only want names of a 
+#specific length. For example, let's say we want a name with 6 or more characters.
 
 #TASK: Use your knowledge of the filter function to create a new dataframe using
 #the "babyNames" dataframe which only contains names with a length greater than
 #or equal to 6. Name this new dataframe "LongbabyNames".
+
+#The function "str_pad" is used to pad out strings to make their lengths or 
+#widths consistent. In this context, "width" refers to display width, the 
+#amount of space the characters actually take on the screen. Here, we'll focus
+#on length. Run the following code:
+
+strlengthtest2 <- str_pad(strlengthtest, 15, "left", pad= " ", use_width = FALSE)
+
+#QUESTION: Dissect the code above. What is the purpose of each part of it?
+
+#TASK: Run str_length on our new vector. Did we successfully make all the lengths
+#consistent?
+
+
+
