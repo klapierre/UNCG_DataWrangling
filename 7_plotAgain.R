ff2ef32cd081b8e523d0f34b5fc9d62ae41c5733
amaliyabrown <amaliya@ad.unc.edu>
2026-03-18 16:21:39 -0400

    Completed part 6.

--- 7_plotAgain.R
+++ 7_plotAgain.R
@@ -642,8 +642,9 @@ data("economics")
 # lines using geom_line().
 # HINT: use ?economics to get more information about this dataset.
 
-
-
+ggplot(economics, aes(x = unemploy, y = date)) +
+  geom_point()+
+  geom_line()
 
 # ---------------------------------------------------------- #
 #### GROUPS AND MAPS                                      ####
