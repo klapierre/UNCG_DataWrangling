e8518c11689e5d0a6dce3a4297a707910a814ab1
katadams1204-star <katadams1204@gmail.com>
2026-03-18 13:42:57 -0400

    Wrote assignment instructions to teach the "str_length" function.

--- Group 2 Project - stringr.R
+++ Group 2 Project - stringr.R
@@ -9,3 +9,26 @@ library(babynames)
 
 #Save the data to its own dataset called babyNames.
 babyNames <- babynames
+
+#Load the stringr package.
+library(stringr)
+
+#MANAGING LENGTHS
+
+#The "str_length" function is a simple function that can tell you the length, in
+#characters, of a string. 
+#Run the following code to create an object we can measure with this function:
+
+strlengthtest <- c("This", "is", "to", "test", "stringr", "length", "functions!")
+
+#TASK: using str_length, determine the amount of characters in each string. If
+#you need help, check the help file for this function.
+
+#The str_length function is useful if we only want to see strings of a certain 
+#character count. It can be combined with the filter function to accomplish this.
+#Suppose we want to pick a baby name, but only want names of a specific length.
+#For example, let's say we want a name with 6 or more characters.
+
+#TASK: Use your knowledge of the filter function to create a new dataframe using
+#the "babyNames" dataframe which only contains names with a length greater than
+#or equal to 6. Name this new dataframe "LongbabyNames".
