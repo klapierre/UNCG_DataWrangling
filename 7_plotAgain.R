b43ee7e86a1567dc09f17a30078976d8e928248d
p-mesha <themeshaparker@gmail.com>
2026-03-18 20:34:39 -0400

    finisihed 1.0

--- 7_plotAgain.R
+++ 7_plotAgain.R
@@ -47,6 +47,7 @@
 # HINT: Check back to last week's assignment section 1.9 for setting themes for
 # all plots.
 
+theme_set(theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()))
 
 # ---------------------------------------------------------- #
 #### 1.0 CORRELATION                                      ####
@@ -64,14 +65,19 @@ data(mpg, package = "ggplot2")
 # HINT: Refer back to last week's assignment or the ggplot help resources if you 
 # forget how to make a scatterplot.
 
+ggplot(data = mpg, aes(x = cty, y = hwy, color = class)) + geom_point() + 
+  labs(x = "City Mileage (MPG)", y = "Highway Mileage (MPG)")
 
 # Looks alright, but the graph may be hiding some information...
 # QUESTION: How many data points are in the mpg dataframe?
 
+#234
 
 # QUESTION: Approximately how many dots are in the graph you just made? How does
 # that compare to the number of observations in the dataframe?
 
+#There are around 80 or 90 dots in the graph and its ledd than the number of actual 
+#observations in the dataframe
 
 # Try another correlation-focused geom that addresses this problem by running
 # the following code:
@@ -81,16 +87,19 @@ ggplot(data=mpg, aes(x=cty, y=hwy)) +
 
 # QUESTION: What happened when you created the plot with geom_jitter?
 
+#The graph showed more dots on the graph than before
 
 # QUESTION: Run the code to create a plot using geom_jitter a second time. Then run it
 # again and again. What happens each time? Why is this happening?
+
+#the dots get randomized, this happens because the jitter code randomizes the dots
 
 
 # TASK: The default in geom_jitter is to jitter (or slightly move) the points away
 # from each other in both the x and y directions. Check the help file for geom_jitter
 # and write code below to make a graph where you jitter points in only the x-dimension
 # by 0.5.
-
+ggplot(data=mpg, aes(x=cty, y=hwy)) +   geom_jitter(width = 0.5, height = 0)
 
 # ---------------------------------------------------------- #
 #### 1.1 DETOUR! COLORS, COLORS, COLORS                   ####
