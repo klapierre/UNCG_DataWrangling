d1a09cbb1e85887bb09798967a7f7e4dd31b9542
hazelrosemarie <bigsisterhrmm@gmail.com>
2026-03-23 14:31:47 -0400

    please

---
+++ animate_group_01.R
@@ -0,0 +1,51 @@
+# Setup -------------------------------------------------------------------
+## Load Packages, Files, Directories
+
+pacman::p_load(tidyverse,
+              gganimate,
+              gapminder,
+              gifski)
+
+# put file paths here
+# tidy or transform data
+
+
+# ggplot2 -----------------------------------------------------------------
+## gganimate is built on ggplot2
+
+# provide some examples of different plot types (relatively complex)
+
+# questions:
+## make a specified graph
+## something about facets (which will become frames)
+## weakness of 2 axes with timeseries data 
+
+
+# gg animate fundamentals -------------------------------------------------
+## Core functions and use cases
+
+# questions:
+## What do specific arguments do?
+## What does changing specific arguments do?
+## What benefits of using animation to include more axes?
+## make an animation
+## save and export an animation with specific size
+
+# making animations more detailed -----------------------------------------
+## expand on gganimate functions
+
+## Transition types
+
+## View types
+
+## Shadows
+
+## Tweening
+
+# Questions:
+
+
+# other useful packages ---------------------------------------------------
+# gifski, magick, gapminder
+
+
