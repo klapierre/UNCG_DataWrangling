e69b6edfa7ef6dad225bfb19e3f2721a213899fe
chloe-houck <chouck03@gmail.com>
2026-03-23 16:21:26 -0400

    Maps assignment up to line 134.

--- 8_Group_maps.R
+++ 8_Group_maps.R
@@ -59,11 +59,21 @@ head(state_data)
 #from the state_data dataframe and name the new dataframe state_frost_data. 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 state_frost_data <- select(state_data, "State", "Frost")
 
 #View the new dataset and confirm that it is correct.
 state_frost_data
 
+=======
+=======
+>>>>>>> Maps assignment up to line 134.
+frost_data <- select(state_data, "State", "Frost")
+
+#View the new dataset and confirm that it is correct.
+frost_data
+<<<<<<< HEAD
+>>>>>>> Maps assignment up to line 134.
 =======
 frost_data <- select(state_data, "State", "Frost")
 
@@ -71,10 +81,6 @@ frost_data <- select(state_data, "State", "Frost")
 frost_data
 >>>>>>> Maps assignment up to line 134.
 =======
-frost_data <- select(state_data, "State", "Frost")
-
-#View the new dataset and confirm that it is correct.
-frost_data
 >>>>>>> Maps assignment up to line 134.
 #Now we need to use the tigris package to download a shapefile called 'states'
 #of all of US states that we would like to plot. We set cb = TRUE 
@@ -103,6 +109,7 @@ states <- states %>% rename(State = NAME)
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 us_states_frost_2 <- left_join(states, state_frost_data, by = c("State"))
 =======
 us_states_frost <- left_join(states, frost_data, by = c("State"))
@@ -131,6 +138,9 @@ us_states_frost <- left_join(states, frost_data, by = c("State"))
 =======
 us_states_frost <- left_join(states, frost_data, by = c("State"))
 >>>>>>> Maps assignment up to line 134.
+>>>>>>> Maps assignment up to line 134.
+=======
+us_states_frost <- left_join(states, frost_data, by = c("State"))
 >>>>>>> Maps assignment up to line 134.
 =======
 us_states_frost <- left_join(states, frost_data, by = c("State"))
@@ -149,6 +159,7 @@ ggplot(us_states_frost) +
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 =======
   ## Creates a plot of the frost days accross the country.
@@ -174,6 +185,9 @@ ggplot(us_states_frost) +
 =======
   ## Creates a plot of the frost days accross the country.
 >>>>>>> Maps assignment up to line 134.
+>>>>>>> Maps assignment up to line 134.
+=======
+  ## Creates a plot of the frost days accross the country.
 >>>>>>> Maps assignment up to line 134.
 =======
   ## Creates a plot of the frost days accross the country.
