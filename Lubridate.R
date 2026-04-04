0100361158f55b862af0ed8926adcce7c492dc8a
amaliyabrown <amaliya@ad.unc.edu>
2026-04-04 11:12:27 -0400

    Updated lubridate assignment.

--- Lubridate.R
+++ Lubridate.R
@@ -2,51 +2,46 @@ library(tidyverse)
 library(lubridate)
 
 # ---------------------------------------------------------- #
-### CONVERTING DATES AND TIMES ####                                           
+### 1.0 CONVERTING DATES AND TIMES ####                                           
 # ---------------------------------------------------------- #
 
-# A Unix, or Epoch, timestamp measures the number of seconds that have passed since January 1st, 1970 at 00:00:00 UTC. The Unix system is useful for computing systems because it stores all time measurements as one large number, rather than more complex formats such as month/day/year. The date-times functions in lubridate allow users to quickly convert Unix measurements to more user-friendly formats.  
+# A Unix timestamp measures the number of seconds that have passed since January 1st, 1970 at 00:00:00 UTC; this is referred to as the 'Epoch'. This is useful for computing systems because it stores all time measurements as one large number, rather than more complex formats such as month/day/year. The date-times functions in lubridate allow users to quickly convert Unix measurements to more user-friendly formats.  
 
 dt_practice <- as_datetime(946684860)
 
 # QUESTION: Run the code above to convert the Unix measurement to ymd_hms format using the ‘as_datetime’ function. What date and time does the timestamp correspond to?
 # ANSWER: "2000-01-01 00:01:00 UTC"
 
-# Unix timestamps are also measured in days since January 1st, 1970. The ‘as_date’ function, like the ‘as_datetime’ function, can be used to convert these measurements to ymd format.
-
+# Unix timestamps are also measured in days since January 1st, 1970. The ‘as_date’ function can be used to convert these measurements to ymd format.
 # QUESTION: What holiday does the 20392 Unix days timestamp correspond to? What year? HINT: Use the ‘as_date’ function to convert days to ymd format. 
 
 dt_holiday <- as_date(20392)
 # ANSWER: Halloween 2025. 
 
 # Unix timestamps can also measure seconds passed since 00:00:00 (with no corresponding date or time zone). The ‘as_hms' function can be used to convert these measurements to hms format. 
-
 # TASK: Run the following code to convert 10,000 seconds to hours. 
 
 print(dt_time <- hms::as_hms(10000))
 
-# QUESTION: How many hours are 86,400 seconds? HINT: Use the ‘as_hms’ function to convert seconds to hms format.
+#QUESTION: How many hours are 86,400 seconds? HINT: Use the ‘as_hms’ function to convert seconds to hms format.
 
 print(dt_time_86 <- hms::as_hms(86400))
 # ANSWER: 24 hours.
 
 # ---------------------------------------------------------- #
-### PARSING DATES AND TIMES ####                                           
+### 1.1 PARSING DATES AND TIMES ####                                           
 # ---------------------------------------------------------- #
 
 # Parsing is the process of converting strings or numbers to into standard date-time format (ymd_hms). This can be done one value at a time, or for an entire column. 
-
-# You can parse date-times with several different functions, so it's important to use the function that corresponds to the order of your data!
-
-# TASK: Run the following lines of code to view how lubridate handles different date-time formats.
+# You can parse date-times with several different functions, so it's important to use the function that corresponds to your input value!
+# TASK: Run the following lines of code to view how lubridate converts different date-time formats.
 
 ymd_hms("2026-04-14 14:00:00")
 ydm_hms("2026-14-04 14:00:00")
 mdy_hms("04/14/2026 14:00:00")
 dmy_hms("14 Apr 2026 14:00:00")
 
-# TIP: If your data only includes dates without timestamsp, you can use the same functions by removing '_hms' from the function. 
-
+# TIP: If your data only includes dates without a timestamsp, you can use the same functions by removing '_hms' from the function. 
 #TASK: Run the following lines of code to convert these dates that lack time values.
 
 ymd("20260414")
@@ -67,13 +62,10 @@ mdy("07-04-12") #ANSWER: The output from 'ydm' is incorrect because the input is
 mdy("November 22nd, 2001") #Example
 
 # Parsing data is super convenient, but what if we want to parse an entire column at once?
-
 #TASK: Load the nycflights13 dataset. This dataset already uses Unix format; run the following code to "break" the dataset so we can practice parsing date-times.
 
 install.packages("nycflights13")
 library(nycflights13)
-
-flights_data <- flights
 
 broken_flights <- flights %>%
   mutate(
@@ -81,7 +73,7 @@ broken_flights <- flights %>%
     flight_time = format(time_hour, "%I: %M: %S %p")) %>% 
   select(flight, tailnum, origin, dest, flight_date, flight_time)
 
-#TASK: Run the following code to convert flight times back to Unix format.
+#TASK: Run the following code to convert flight times back to Unix format in the flight_data_parsed dataframe.
 
 flight_data_parsed <- broken_flights %>% mutate(flight_time = hms::as_hms(flight_time))
 
@@ -90,24 +82,17 @@ flight_data_parsed <- broken_flights %>% mutate(flight_time = hms::as_hms(flight
 
 flight_data_parsed <- broken_flights %>% mutate(flight_date = mdy(flight_date)) #Example
 
-class(flight_data_parsed$flight_time) #Check
-class(flight_data_parsed$flight_date) #Check
-
 # ---------------------------------------------------------- #
-### GETTING AND SETTING DATES AND TIMES ####                        
+### 1.2 GETTING AND SETTING DATES AND TIMES ####                        
 # ---------------------------------------------------------- #
 
-# Before exploring the rounding functions in lubridate, lets check out one more useful function for setting a date-time value.
-
-# The 'now()' function allows us to find the date-time value for this exact moment in time. 
-
+# Before exploring more functions in lubridate, lets check out one more useful function for setting a date-time value.The 'now()' function allows us to find the date-time value for this exact moment in time. 
 #TASK: Run the following code to create the 'todays_timestamp' object with the 'now()' function.
 
 todays_timestamp <- now()
 
-# Lubridate is also helpful for finding specific components of date-time data.
-
-#TASK: Run the following lines of code to isolate specific components of the date-time value. 
+# Lubridate helpful for finding specific components of date-time data.
+# TASK: Run the following lines of code to isolate specific components of the date-time value. 
 
 date(todays_timestamp)
 year(todays_timestamp)
@@ -124,7 +109,7 @@ wday(todays_timestamp)
 
 #ANSWER: week of the year and day of the week
 
-#TASK: What if we wanted to create new column in our flights dataset listing the day of the week of the flight? Run the following code to create a flight_day column in out flight_data_parsed dataset.
+#TASK: What if we wanted to create new column in our flights dataset listing the day of the week of the flight? Run the following code to create a flight_day column in our flight_data_parsed dataset.
 
 flight_data_parsed <- flight_data_parsed %>% mutate(flight_day = wday(flight_date, label = TRUE, abbr = FALSE))
 
@@ -132,16 +117,12 @@ flight_data_parsed <- flight_data_parsed %>% mutate(flight_day = wday(flight_dat
 # HINT: Try running the code without the 'label' and 'abbr' arguments.
 #ANSWER: Label = displaying the day of the week as a name (not number). Abbr = abbreviation.
 
-#TASK: Create a new column called 'flight_month' in the flight_data_parsed dataset that lists the names of the month the flight took place. 
+#TASK: Create a new column called 'flight_month' in the flight_data_parsed dataset that lists the names of the months that the flights took place. 
 
-flight_data_parsed <- flight_data_parsed %>% mutate(flight_month = month(flight_date, label = TRUE, abbr = FALSE))
-
-#TASK: What if we only wanted to look at flights leaving in the AM? Run the following code to create a new column that lists whether flights are AM or PM.
-
-flight_data_parsed <- flight_data_parsed %>% mutate(flight_AMPM = am(flight_time))
+flight_data_parsed <- flight_data_parsed %>% mutate(flight_month = month(flight_date, label = TRUE, abbr = FALSE)) #Answer
 
 # ---------------------------------------------------------- #
-### ROUNDING DATES AND TIMES ####                                  
+### 1.3 ROUNDING DATES AND TIMES ####                                  
 # ---------------------------------------------------------- #
 
 # There might be times where you want to round your data. For example, what if you wanted to round to the nearest month? 
@@ -163,11 +144,7 @@ round_date(mdy("April 15 2026"), "month")
 
 rollback(todays_timestamp, roll_to_first = FALSE, preserve_hms = TRUE)
 
-# ---------------------------------------------------------- #
-### PUTTING IT ALL TOGETHER ####                                  
-# ---------------------------------------------------------- #
-
 # TASK: Create a dataframe named 'flight_data_cleaned' that includes a column named 'rounded_flight_data' that rounds the flight date to the nearest month. 
 
-flight_data_final <- flight_data_parsed %>% 
-  mutate(rounded_flight_date = round_date(mdy(flight_date), "month"))
+flight_data_cleaned <- flight_data_parsed %>% 
+  mutate(rounded_flight_date = round_date(ymd(flight_date), "month")) # ANSWER
