72a81e517d4786abb9b7831c450807c390ddb561
michellebcxo <michellebc924@gmail.com>
2026-04-06 07:09:23 -0400

    I ran seven str functions (upper,lower,title,sentence,camel,snake, and kebab) on case_names. I created a task using babynames dataframe to make a new dataset uppercaseBabyNames with year and name_upper columns. In name_upper, all names are capitalized. Using babyname dataframe I created a new dataset oldBabyNames. Showing baby names from 1880. First and last names begin with a capital letter.

--- 9_stringr.R*.R
+++ 9_stringr.R*.R
@@ -109,29 +109,31 @@ case_names <- c("layne staley", "JERRY CANTRELL", "MIKE inez", "SeAn KiNnEy")
 ## TASK: Run the following seven functions, and answer the question at the end of the list of functions either during or after this process.
 
 str_to_upper(case_names)
-
+#All capital letters 
 str_to_lower(case_names)
-
+#All lowercase letters
 str_to_title(case_names)
-
+#First letter for each first and last name is capitalized
 str_to_sentence(case_names)
-
+#Only first letter is capitalized in each first name, and the rest is lowercase
 str_to_camel(case_names)
-
+#Each first word is lower-cased. Each subsequent word starts with a capital letter.No separation between words.
 str_to_snake(case_names)
-
+#Words are converted to lowercase and separated by underscores.
 str_to_kebab(case_names)
-
+#Words are lowercased and separated by dashes
 ## QUESTION: What happened to the case_names values in reference to each function that you ran?
-
+#Done.
 ## When just doing the camel, snake, and kebab functions, the last name came out weird, right? Let's fix that.
 
 ## TASK: Put the uppercase function within the camel function and run the code. Then, instead of the uppercase function, put the lowercase function within the camel function and run the code.
+str_to_camel(str_to_upper(case_names))
+str_to_camel(str_to_lower(case_names))
 
 ## QUESTION: What do you notice about the two results?
-
+#Both results are the same. The first word lowercased and each subsequent word starts with a capital letter. No separation between words.
 ## QUESTION: Since the last name in case_names had problems with the camel, snake, and kebab functions, is it possible to also fix the name with the snake and kebab functions as we did with the camel function in the previous task?
-
+#Yes, its possible.
 ## Now that you've completed some examples using case_names, lets use our babyNames data frame to modify something a little more complex.
 
 ## TASK: Using the babyNames data frame, complete the following:
@@ -139,13 +141,19 @@ str_to_kebab(case_names)
 #(2) Select by name and year.
 #(3) Mutate the name column to be labelled name_upper and use the                   uppercase function on the name column.
 #(4) Select the name column once again, removing it, leaving only the               name_upper column.
-
+uppercaseBabyNames<-babynames %>% 
+  select(name, year) %>% 
+  mutate(name_upper=str_to_upper(name)) %>% 
+  select(-name)
 ## TASK: Using the babyNames data frame, complete the following:
 #(1) Create a new data frame named oldBabyNames.
 #(2) Filter by the year 1880
 #(3) Mutate the name column to be labelled name_title and use the title             function on the name column.
 #(4) Select the name column once again, removing it, leaving only the               name_title column.
-
+oldBabyNames<-babynames %>% 
+  filter(year==1880) %>% 
+  mutate(name_title=str_to_title(name)) %>% 
+  select(-name)
 ## Good work! You've learned how to use some case changing functions within simple values as well as data frames!
 
 ## These examples might not be the most practical, but the option to organize entire data frames within simple functions justifies its use in a more practical sense.
