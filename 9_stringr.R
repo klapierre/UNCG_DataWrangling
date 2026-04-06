ee002cc13892e9e1d7941266373cffe78fbc517c
kmclark4 <kmclark4@uncg.edu>
2026-04-05 23:23:03 -0400

    Stringr Part 1.3

--- 9_stringr.R
+++ 9_stringr.R
@@ -205,6 +205,7 @@ str_which(fruit, "q")
 
 ## QUESTION: What is the result of running this code?
 
+#ANSWER:43 46 67
 
 ## Not only can we use the str_detect function to detect individual characters in
 ## a string, but we can also use it to detect larger patterns of characters. For 
@@ -219,6 +220,7 @@ str_detect(fruit,"melon")
 ## TASK: Try using the str_detect function with "(.)\\1" as the pattern to find 
 ## out if there are any fruits containing double letters.
 
+str_detect(fruit, "(.)\\1")
 
 ## Another interesting function is str_count.
 ## TASK: Run the following code to create a smaller vector containing only the
@@ -229,6 +231,7 @@ str_count(fivefruits, "a")
 
 ## QUESTION: What does the str_count function do? If needed, use ?str_count.
 
+#ANSWER: it counts how many letters that we asked in each string. for example it counted how many 'a' were in each work of the first five fruits. 
 
 ## To get the actual position of the first occurrence of the letter "a" in each of
 ## these five fruits, we can run the following code.
@@ -241,6 +244,7 @@ str_locate(fivefruits, "a")
 
 ## QUESTION: Notice that the fifth fruit returns NAs. Why do you think this is?
 
+#ANSWER: There are no a's in that word.
 
 ## This tells us only the first occurrence of the letter "a" in each of the five 
 ## fruits. However, if we wanted to locate the positions of all of the matches 
@@ -249,19 +253,26 @@ str_locate_all(fivefruits, "a")
 
 ## QUESTION: Which positions contain "a" in the fourth fruit in our vector? 
 ## (Hint: There are three.)
-
+ 
+ #ANSWER: the 2nd, fourth,and 6th letters are a's.
 
 ## QUESTION: Other functions that are similar to str_detect are str_starts and 
 ## str_ends. What do you think each of these does?
 
+#str_detect() Returns TRUE if a pattern is detected
+#str_starts() specialized version of str_dectect. Only looks at the beginning.
+#str_ends() specialized version of str_dectect. Only looks at the end.
 
 ## TASK: Use the knowledge you've learned in this section to create a code that gives 
 ## a count of how many fruits in the fruit vector contain the word "berry". (Hint:
 ## You will need to use a dplyr function.)
 
+fruit %>% 
+  str_detect("berry") %>% 
+  sum()
 
 ## QUESTION: How many fruits containing the word "berry" are there in the fruit vector?
-
+#ANSWER: 14
 
 ## Great job! You've learned how to use stringr to detect, count, and locate 
 ## pattern matches in strings of characters. One way that these functions could 
