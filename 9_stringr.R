226b1bb25516d0492f7da26595e22dabc9fc00ac
kmclark4 <kmclark4@uncg.edu>
2026-04-05 23:47:12 -0400

    Part 1.6 (?)

--- 9_stringr.R
+++ 9_stringr.R
@@ -404,20 +404,37 @@ Male_Vowelless <- str_count(NYC_Baby_Names_2021_List[["MALE"]]$Childs_First_Name
 taco_ingredients <- str_c("tortilla","beans", "lettuce", "guacamole", "cheese", "salsa")
 
 ## QUESTION: Print the vector below. Is it legible? Why or why not?
-
+taco_ingredients
+#ANSWER: all the words run together.
 
 ## TASK: Now make the same vector, but formatted as a list (as in with ", "
 ## after each word).
+
+taco_ingredients <- str_c("tortilla", "beans", "lettuce", "guacamole", "cheese", "salsa", sep = ", ")
 
 
 ## TASK: Not everyone likes cilantro, so use the replace function to
 ## replace it with guacamole and save it as taco_ingredients_2
 
+taco_ingredients_2 <- str_replace(taco_ingredients, "cilantro", "rice") # I don't like cilantro or guac so I'm replacing it with rice. 
+
 
 ## TASK: Now, it appears that we have forgotten to include meat in the list.
 ## Add meat to the list after the tortilla, but before the beans.
+
+taco_ingredients <- str_c(
+  "tortilla",
+  "chicken",
+  "beans",
+  "lettuce",
+  "rice",
+  "cheese",
+  "salsa",
+  sep = ", ")
 
 
 ## TASK: Now remove one ingredient of choice without replacing it with anything.
 ## Save this as taco_ingredients_3 without any empty strings.
 
+taco_ingredients_3 <- str_replace(taco_ingredients, ", beans", "")
+taco_ingredients_3
