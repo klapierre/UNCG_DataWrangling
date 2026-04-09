f2d64beeeb52ad47733575916854a2a38f01ddef
dhanush0012 <dhanush.devanand@gmail.com>
2026-04-08 20:05:40 -0400

    Lengths

--- 9_stringr.R
+++ 9_stringr.R
@@ -32,6 +32,8 @@ strlengthtest <- c("This", "is", "to", "test", "stringr", "length", "functions!"
 ##TASK: using str_length, determine the amount of characters in each string. If
 ##you need help, check the help file for this function.
 
+str_length(strlengthtest)
+
 ##The str_length function is useful if we only want to see strings of a certain 
 ##character count. It can be combined with the filter function to accomplish this
 ##in a dataframe.Suppose we want to pick a baby name, but only want names of a 
@@ -42,6 +44,9 @@ strlengthtest <- c("This", "is", "to", "test", "stringr", "length", "functions!"
 ##or equal to 6. Name this new dataframe "LongbabyNames". (Note: you will need
 ##to load the dplyr package to use the filter function.)
 
+LongbabyNames <- babyNames %>%
+  filter(str_length(name) >= 6)
+
 ##The function "str_pad" is used to pad out strings to make their lengths or 
 ##widths consistent. In this context, "width" refers to display width, the 
 ##amount of space the characters actually take on the screen. Here, we'll focus
@@ -51,8 +56,16 @@ strlengthtest2 <- str_pad(strlengthtest, 15, "left", pad= " ", use_width = FALSE
 
 ##QUESTION: Dissect the code above. What is the purpose of each argument in it?
 
+##strlengthtest = the vector being changed
+##15 = makes each string length 15 characters
+##"left" = adds padding on the left side
+##pad = " " = uses spaces as the padding character
+##use_width = FALSE = uses character length, not display width
+
 ##TASK: Run str_length on our new vector. Did we successfully make all the lengths
 ##consistent?
+
+str_length(strlengthtest2)
 
 ##"str_trim" essentially reverses what we did with str_pad; it removes white
 ##space from strings. Run this code:
@@ -61,6 +74,8 @@ strlengthtrimmed <- str_trim(strlengthtest2)
 
 ##QUESTION: What is a possible practical application of the str_pad and str_trim
 ##functions?
+
+##Cleaning text to have it line up the way you want, remove spaces, trim things up.
 
 ##The final length management function we will learn is "str_trunc". This function
 ##also serves to make string lengths consistent, but it does this by chopping off
@@ -71,9 +86,14 @@ str_trunc(strlengthtest, 4)
 
 ##QUESTION: How long is each string in the vector now?
 
+##AT MOST 4 characters long
+
 ##TASK: Write code to create a new dataframe called "babyNamesTrunc". In this 
 ##dataframe, truncate the "name" column so that each string is at most 5 
 ##characters. HINT: The mutate function will be useful here.
+
+babyNamesTrunc <- babyNames %>%
+  mutate(name = str_trunc(name, 5))
 
 #-----------------------------------------#
 #### PART 1.2: CHANGING CASE
