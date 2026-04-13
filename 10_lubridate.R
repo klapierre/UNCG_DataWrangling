3acbfd8827f9cddc91a1d7e44124351bf966e15b
dylanmarlowe <dylanmarlowe03@gmail.com>
2026-04-13 14:44:30 -0400

    Completed 1.1 in lubridate assignment.

--- 10_lubridate.R
+++ 10_lubridate.R
@@ -80,11 +80,20 @@ ydm("07-04-12")
 
 # QUESTION: Is the output value correct? Why not? Rewrite the code with the same input using the correct function below. 
 
+## Should be this:
+ydm("07-12-04")
+
+
+
 mdy("07-04-12") 
 
 #TASK: Use any of the parsing functions to convert your birthday into standard date format in the space below. 
 
 mdy("November 22nd, 2001") #Example
+
+mdy("August 3rd, 2003")
+  
+  
 
 # Parsing data is super convenient, but what if we want to parse an entire column at once?
 #TASK: Load the nycflights13 dataset. Run the following code to "break" the dataset (turning the timestamps into strings) so we can practice parsing date-times.
