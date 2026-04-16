21a7603b03fd945de0e91c7dd237a072216969fe
chloe-houck <chouck03@gmail.com>
2026-04-15 23:53:01 -0400

    Getting rid of some random characters that came out of nowhere.

--- 8_Group_maps.R
+++ 8_Group_maps.R
@@ -57,10 +57,11 @@ head(state_data)
 #Task: Because we have loaded the dyplr package, we can create a new dataframe
 #with just State and Frost data. Write a code to select the State and Frost columns
 #from the state_data dataframe and name the new dataframe state_frost_data. 
-frost_data <- select(state_data, "State", "Frost")
+state_frost_data <- select(state_data, "State", "Frost")
 
 #View the new dataset and confirm that it is correct.
-frost_data
+state_frost_data
+
 #Now we need to use the tigris package to download a shapefile called 'states'
 #of all of US states that we would like to plot. We set cb = TRUE 
 #to specify cartographic boundary type so that we use a simple shapefile that
@@ -84,6 +85,7 @@ states <- states %>% rename(State = NAME)
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 us_states_frost_2 <- left_join(states, state_frost_data, by = c("State"))
 =======
 us_states_frost <- left_join(states, frost_data, by = c("State"))
@@ -97,6 +99,9 @@ us_states_frost <- left_join(states, frost_data, by = c("State"))
 =======
 us_states_frost <- left_join(states, frost_data, by = c("State"))
 >>>>>>> Maps assignment up to line 134.
+=======
+us_states_frost_2 <- left_join(states, state_frost_data, by = c("State"))
+>>>>>>> Getting rid of some random characters that came out of nowhere.
 
 #Next we will use ggplot to construct a map of the US States and number of Frost
 #days
@@ -107,6 +112,7 @@ ggplot(us_states_frost) +
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
   ## Creates a plot of the frost days accross the country.
 >>>>>>> Maps assignment up to line 134.
@@ -119,6 +125,10 @@ ggplot(us_states_frost) +
 =======
   ## Creates a plot of the frost days accross the country.
 >>>>>>> Maps assignment up to line 134.
+=======
+
+  ## Creates a plot of the frost days accross the country.
+>>>>>>> Getting rid of some random characters that came out of nowhere.
   geom_sf(aes(fill = Frost), color = "blue") +
   ## Uses the color blue as an indicator of number of frost days.
   theme_minimal() +
