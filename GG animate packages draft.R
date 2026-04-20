e31b38debec3023230a6dd1b54fb08bb51882524
jadisvang04 <jadisvang04@gmail.com>
2026-04-20 02:06:57 -0400

    Continuation of creating the code for the assignment. ---
    1) Settled and tidied imported data Time vs Temp
    2) Gapminder coding
    3) Gifski *incomplete

--- GG animate packages draft.R
+++ GG animate packages draft.R
@@ -39,7 +39,77 @@ remove(cells_Clean)
 
 
 -----
+  
+library(tidyverse)
 
-temp <- read.csv("temperature_data_1.cvs")
+temp <- read.csv("temperature_data_1.csv")
+  
+streamTemp <- temp %>%
+  drop_na() %>%
+  separate(Date, into = c("date", "time"), sep = " ") %>%
+  mutate(date = as.Date(date, format = "%m/%d/%Y"),time = hms::as_hms(time))
+
+timeTemp <- streamTemp %>%
+  select(-time, -Hour, -Minute, -X)
+
+dailyAvg <- streamTemp %>%
+  group_by(date) %>%
+  summarize(mean_temp = mean(Air.temperature, na.rm = TRUE)) %>%
+  ungroup()
+
+--------------------------------
+ Plotting Time vs temperature:
+--------------------------------
+
+ggplot(dailyAvg, aes(x = mean_temp)) +
+  geom_histogram(binwidth = 1, fill = "skyblue", color = "black") +
+  labs(
+    x = "Daily Average Temperature",
+    y = "Count of Days",
+    title = "Histogram of Daily Average Temperatures"
+  ) +
+  theme_bw()
+
+
+--------------------------------
+  Gapminder
+--------------------------------
+
+ggplot(dailyAvg, aes(x = date, y = mean_temp)) +
+  geom_line(color = "steelblue", linewidth = 1.2) +
+  geom_point(color = "orange", size = 2) +
+  labs(
+    x = "Date",
+    y = "Daily Average Temperature",
+    title = "Daily Average Temperature Over Time (Gapminder Style)"
+  ) +
+  theme_bw() +
+  theme(
+    plot.title = element_text(size = 14, face = "bold"),
+    axis.title = element_text(size = 12)
+  )
+
+--------------------------------
+  Gifski
+--------------------------------
+
+# Gif are the main priotity of this package! 
+  
+# FOR CLARITY: Gifski does not animate the date itself. It takes photos (PNG's) created from the date and combine them in a presentation compressed into a gif. Like a wheel of photos. 
+  
+# To start creating a GIF, we need to convert our data into PNGs. What would be a good parameter for the photo creation? 
+# HINT: Look at the columns made 
+
+
+
+str(timeTemp)
+
+daily_temp <- df %>%
+  group_by(date) %>%
+  summarize(mean_temp = mean(temp, na.rm = TRUE))
+
+ggplot(daily_temp, aes(x = date, y = mean_temp)) +
+  geom_line() +
+  geom_point()
 
 
