e0ac656756456a434683ff2560600fdba5240fc9
dylanmarlowe <dylanmarlowe03@gmail.com>
2026-04-29 14:21:18 -0400

    Completed the preparation stage of the project.

---
+++ Independant Project - Dylan Marlowe.R
@@ -0,0 +1,42 @@
+### ------ Preparation ------ ###
+
+
+## Obtaining resources:
+
+# The data used is within this link:
+# https://datadryad.org/dataset/doi:10.5061/dryad.8c4092n
+
+# Download the .csv file at the top of the page (under "Data files"). 
+# After downloading the file, it should look like this:
+
+Vagi+et+al_S3+Supplementary+data.csv
+
+# Read through the complete article through this DOI link:
+# (This is optional for the viewer)
+# https://royalsocietypublishing.org/rspb/article/286/1900/20182737/85002/Parental-care-and-the-evolution-of-terrestriality
+
+
+## Set up the resources and packages needed to begin this project:
+
+library(tidyverse)
+
+# Here, we are just loading the "tidyverse" package.
+
+# "tidyverse" is a key package in tidying our data, as it contains many functions that allow us to alter and visualize our data in future steps.
+
+untidy_data <- read.csv2("Vagi+et+al_S3+Supplementary+data.csv")
+
+# Here, we are creating a dataframe that we can use as a baseline for the rest of this project, out of the data we downloaded earlier.
+
+# So, if you opened the .csv file you downloaded straight from your file folder, you would notice that everything seemed smashed into one column.
+
+#We have a problem before we can even start.
+
+# The "read.csv" function does not work, so we need to use the "read.csv2" function. 
+
+# The "read.csv" function reads through comma-separated files while the "read.csv2" function reads through semicolon-separated files.
+
+# Basically, we had to download a .csv file of a .csv2 file, pushing us to re-read it as such within R Studio.
+
+
+### ------ Step 1 ------ ###
