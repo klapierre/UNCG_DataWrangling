4c4b3b2de1f519ff7a2edf7f0e8563640acf47d8
dylanmarlowe <dylanmarlowe03@gmail.com>
2026-04-30 03:36:13 -0400

    Independant Project - Hopefully completed the coding for the data portion. I also added in comments to each step and further organized comments. Next up is coding figures.

--- Independant Project - Dylan Marlowe.R
+++ Independant Project - Dylan Marlowe.R
@@ -57,15 +57,18 @@ untidy_data <- read.csv2("Vagi+et+al_S3+Supplementary+data.csv")
 
 # At the same time, we don't want to delete the rest of the data, so we will just separate them into three distinct datasets.
 
+
 names_data <- untidy_data[, c("Species", "Family", "Clade")]
 
 # This will create a dataframe specific to the names.
+
 
 untidy_char_num_data <- untidy_data[, !names(untidy_data) %in% c("Family", "Clade", "Parental.care.references", "Terrestrial...aquatic.reproduction.references", "Direct.development.references", "Female.size.references", "Male.size.references", "Egg.size.references", "Clutch.size.references")]
 
 # This will create a dataframe specific to the character/numerical data.
 
 # When creating a new dataframe for the character/numerical data, it was easier choosing what to remove than what to add.
+
 
 references_data <- untidy_data[, c("Species", "Parental.care.references", "Terrestrial...aquatic.reproduction.references", "Direct.development.references", "Female.size.references", "Male.size.references", "Egg.size.references", "Clutch.size.references")]
 
@@ -81,9 +84,10 @@ references_data <- untidy_data[, c("Species", "Parental.care.references", "Terre
 ### ------ Tidying Our Character/Numerical Data ------ ###
 
 
+
 ## -- Part 1: Dropping Unused Columns -- ##
 
-tidy_data_part_1 <- untidy_char_num_data %>%
+td_drop_cols <- untidy_char_num_data %>%
   select(-care_in_males_binary,
          -care_in_females_binary,
          -nourishment_by_females,
@@ -91,10 +95,14 @@ tidy_data_part_1 <- untidy_char_num_data %>%
          -protection,
          -care_duration_in_males,
          -care_duration_in_females)
-  
+
+# The first step I'm making is to remove columns that I have no use for.
+# The columns removed are ones that will end up redundant later.
+
+
 ## -- Part 2: Pivoting Type of Care Column -- ##
 
-tidy_data_part_2 <- tidy_data_part_1 %>%
+td_type_of_care <- td_drop_cols %>%
   filter(!is.na(type_of_care)) %>%
   mutate(value = 1) %>%
   pivot_wider(names_from = type_of_care,
@@ -106,9 +114,23 @@ tidy_data_part_2 <- tidy_data_part_1 %>%
          `biparental_care` = `biparental care`) %>%
   select(-"either parent")
 
+# Here, I'm pivoting the "type_of_care" column for a couple reasons:
+# 1.) Remove character values from observations
+# 2.) Transition to a binary system
+
+# I am also removing one set of the observation values that would've become a column (either parent).
+# This is because the article had explained that there were too few examples of this being used, so it wasn't even used in further analysis.
+
+# I want to make an important point that you will see a couple instances of me filtering out NA values, while the end result will still carry them.
+# This is due to the severity of the outcome.
+# Instances where I removed NA values barely impacted the overall observation count.
+# There were times where up to 80% of the observation count was removed due to filtering for NA values in certain columns.
+# As much as I wanted to remove NA values entirely to make the data more tidy, it wasn't worth completely destroying the data.
+
+
 ## -- Part 3: Pivoting Direct Development Column -- ##
 
-tidy_data_part_3 <- tidy_data_part_2 %>%
+td_direct_development <- td_type_of_care %>%
   mutate(value = 1) %>%
   pivot_wider(names_from = direct_development,
               values_from = value,
@@ -116,9 +138,13 @@ tidy_data_part_3 <- tidy_data_part_2 %>%
   rename(full_body_development = present,
          tadpole_development = absent)
 
+# Once again, we are pivoting to change our observations to become column titles to allow for a binary system. 
+# It's basically just another addition to the list.
+
+
 ## -- Part 4: Pivoting Care Duration Column -- ##
 
-tidy_data_part_4 <- tidy_data_part_3 %>%
+td_duration <- td_direct_development %>%
   filter(!is.na(care_duration)) %>%
   mutate(value = 1) %>%
   pivot_wider(names_from = care_duration,
@@ -129,9 +155,17 @@ tidy_data_part_4 <- tidy_data_part_3 %>%
          parental_tadpole_care = `2`,
          parental_juvenile_care = `3`)
 
+# This is very similar to the other cases so far. We see the filtering on NA values, as well as another pivot.
+
+# I am removing a column "0", but what is it?
+# "0" was created because we are using a number system from 0-3, but are trying to make those numbers the columns and make it a binary system. 
+# The problem was that "0" represented something I had already done "no care", so I didn't want to include it. 
+# To account for the 0s, R made its own "0" column. So, I just removed it.
+
+
 ## -- Part 5.1: Creating Male Protection Data -- ##
 
-tidy_data_males <- tidy_data_part_4 %>%
+td_male_protection <- td_duration %>%
   filter(!is.na(protection_in_males)) %>%
   mutate(value = 1) %>%
   pivot_wider(names_from = protection_in_males,
@@ -148,9 +182,21 @@ tidy_data_males <- tidy_data_part_4 %>%
          "male_back_carrying", 
          "male_internal_carrying")
 
+# This is where we see similar steps, but begin to take different actions.
+
+# Basically, there is a 0-5 scale for both male and female protection of their children. So, we are creating a lot of columns to make make binary observations out of.
+# In the male version, we seem to be missing number 5, right?
+# This is intentional, as there were no cases of male viviparity.
+
+# It should be known that we are taking a detour here, but a required one.
+# I initially tried to have male and female protection go straight into the main dataframe, but I kept getting an error about the same name.
+# So, it forced me to split up the process, selecting certain columns.
+# Note that I kept the Species column to call back between dataframes.
+
+
 ## -- Part 5.2: Creating Female Protection Data -- ##
 
-tidy_data_females <- tidy_data_part_4 %>%
+td_female_protection <- td_duration %>%
   filter(!is.na(protection_in_females)) %>%
   mutate(value = 1) %>%
   pivot_wider(names_from = protection_in_females,
@@ -168,17 +214,87 @@ tidy_data_females <- tidy_data_part_4 %>%
          "female_back_carrying",
          "female_internal_carrying",
          "female_viviparity")
-  
+
+# This is just as I had explained with the male protection segment.
+
+# You can see we now have female viviparity, where male viviparity was nonexistent.
+
+#Besides that and the names being "female" instead of "male", everything else is the same.
+
+
 ## -- Part 5.3: Combining Male and Female Protection Data -- ##
 
-tidy_data_m_f <- left_join(tidy_data_males,
-                           tidy_data_females,
-                           by = "Species")
+td_male_and_female <- left_join(td_male_protection,
+                                td_female_protection,
+                                by = "Species")
+
+# This is where we take our male and female segments and joing them together, using "Species" as the glue. 
+
 
 ## -- Part 5.4: Reintegrating Protection Data Into Main Dataframe -- ##
 
-tidy_data_part_5 <- left_join(tidy_data_m_f,
-                              tidy_data_part_4,
+td_reintegration <- left_join(td_male_and_female,
+                              td_duration,
                               by = "Species") %>%
   select(-c("protection_in_males",
             "protection_in_females"))
+
+# This is where we take the joined male and female segment, and reintegrate it with the main dataframe, using "Species" as the glue.
+# Now, we're removing a couple of columns, but don't worry, they are the columns we used to make the male and female segments work, so they are nothing more than redundant now.
+
+
+## -- Part 6: Renaming and Reordering -- ##
+
+td_rename_reorder <- td_reintegration %>%
+  rename_with(~ str_to_lower(.)) %>%
+  rename(male_parental_care = `male_parental care`,
+         female_parental_care = `female_parental care`) %>%
+  select(species,
+         number_of_parents,
+         no_parental_care,
+         male_parental_care,
+         female_parental_care,
+         biparental_care,
+         parental_egg_care,
+         parental_tadpole_care,
+         parental_juvenile_care,
+         male_nest_protection,
+         male_parental_attendance,
+         male_back_carrying,
+         male_internal_carrying,
+         female_nest_protection,
+         female_parental_attendance,
+         female_back_carrying,
+         female_internal_carrying,
+         female_viviparity,
+         tadpole_development,
+         full_body_development,
+         terrestrial_reproduction,
+         female_svl,
+         male_svl,
+         average_male_female_svl,
+         ssd,
+         egg_diameter,
+         log_egg_diameter,
+         clutch_size,
+         log_clutch_size,
+         clutch_volume,
+         log_clutch_volume)
+
+# So, not everything was amazingly perfect at this point, so I felt it was best to do some renaming and reorganizing.
+
+# I decided to make everything lowercase, since some terms like SSD and SVL would be difficult to work with unless making everything uppercase or individually changing every column name. 
+
+# I had to manually rename a couple of column names because they carried syntax errors when running the code with the spaces they had.
+
+# On top of that, every time we pivoted and inserted new material, we ended up moving those columns to the right of the table.
+# To fix this, I manually reorganized each column to how I thought would be best.
+
+# Alright, now we have a data set that is organized in it's values and observations. While there are still remaining NA values, I'd rather preserve the data rather than try and scrub it too clean.
+
+
+
+### ------ Creating Figures ------ ###
+
+
+
