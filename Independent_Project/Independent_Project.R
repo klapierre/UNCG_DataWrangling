53402e23f5543b8000b580f671ad51db764be134
chloe-houck <chouck03@gmail.com>
2026-05-04 21:51:35 -0400

    More Independent Project Stuff.

--- Independent_Project/Independent_Project.R
+++ Independent_Project/Independent_Project.R
@@ -1,10 +1,11 @@
-Average_US_Temps <- read.csv("Average Temperature 1900-2023.csv")
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+Average_US_Temps <- read.csv("Average Temperature 1900-2023.csv")
+Average_US_Temps <- rename(Average_US_Temps, "Temperatures" = "Average_Fahrenheit_Temperature")
 
 ggplot(data = Average_US_Temps,
-       aes(x = Year, y = Average_Fahrenheit_Temperature),) +
+       aes(x = Year, y = Temperatures)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE)+
   labs(title = "Average US Temperatures by Year", x = "Year", y = "Average Temperature (°F)")+
@@ -14,4 +15,14 @@ ggplot(data = Average_US_Temps,
     panel.background = element_rect(fill = "grey"),
     legend.background = element_rect(fill = "grey")
   )
-  
+Rearanged_Temps <- column_to_rownames(Average_US_Temps, var = "Year")
+ggplot(data = Average_US_Temps,
+       aes(x = Year, y = Temperatures)) +
+  geom_col() +
+  labs(title = "Average US Temperatures by Year", x = "Year", y = "Average Temperature (°F)")+
+  theme_dark() +
+  theme(
+    plot.background = element_rect(fill = "grey", colour = NA),
+    panel.background = element_rect(fill = "grey"),
+    legend.background = element_rect(fill = "grey")
+  )
