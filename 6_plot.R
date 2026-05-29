b1faefdad727938938e87e1197ac7db45030d2fd
chloe-houck <chouck03@gmail.com>
2026-05-29 18:13:58 -0400

    Assignment 6 part 1.5 done.

--- 6_plot.R
+++ 6_plot.R
@@ -261,7 +261,7 @@ ggplot(redband, aes(x = Length, y = Weight)) +
 # QUESTION: Using the geom_smooth help page, what type of function is being used 
 # in the above graph for our statistical transformation fit?
 # HINT: What is the default model type for a dataframe of our size?
-
+## The function used is a scatter plot with a trend line included.
 
 # We also can specify a specific model to fit. Try running the following code to
 # specify a linear model:
@@ -272,7 +272,9 @@ ggplot(redband, aes(x = Length, y = Weight)) +
 # TASK: As with most things in R, there are multiple ways to accomplish the same
 # task. Using the geom_smooth help page, write code below to specify a linear
 # model using a method= statement instead of the formula= statement.
-
+ggplot(redband, aes(x = Length, y = Weight)) + 
+  geom_point() + 
+  geom_smooth(method = lm)
 
 # A linear model does not seem like a good fit to our data. Try running the
 # following code to generate a quadratic model.
@@ -292,7 +294,8 @@ ggplot(redband, aes(x = as.factor(ScaleAge), y=Weight)) +
 # QUESTION: Name another statistical transformation we have already used in this
 # assignment.
 # HINT: It was in the very first part of the assignment.
-
+## Histograms were used to generate a graph of the distribution for each value of
+## a given variable.
 
 # TASK: Let's put this all together! Create a graph with the following:
 # (1) redband dataframe,
@@ -300,7 +303,9 @@ ggplot(redband, aes(x = as.factor(ScaleAge), y=Weight)) +
 # (3) points colored by ScaleAge as a factor
 # (4) quadratic line that is black in color and size=2 (HINT: check ggplot
 #     cookbook to help figure out how to change line color and size).
-
+ggplot(redband, aes(x=Length, y=Weight))+
+  geom_point(aes(colour = as.factor(ScaleAge)))+
+  geom_smooth(formula = y ~ poly(x,2), color = "black", size = 2)
 
 # ---------------------------------------------------------- #
 #### PART 1.6 DATA IN VS DATA OUT                         #### 
